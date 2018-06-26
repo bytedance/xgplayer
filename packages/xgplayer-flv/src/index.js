0d8765f5b880b454a642811ab13e0bee886e6d95
fuyuhao <fuyuhao@bytedance.com>
2018-06-26 17:25:55 +0800

    fix(flv/MainParser): 解决flv连续seek导致的视频播放错误
    fix #10

--- packages/xgplayer-flv/src/index.js
+++ packages/xgplayer-flv/src/index.js
@@ -1,79 +1,79 @@
-import MainParser from './parse/MainParser';
-import Player from 'xgplayer';
-import MSE from './parse/MSE';
-import VodTask from './utils/VodTask';
-import defaultConfig from './constants/config';
+import MainParser from './parse/MainParser'
+import Player from 'xgplayer'
+import MSE from './parse/MSE'
+import VodTask from './utils/VodTask'
+import defaultConfig from './constants/config'
 
 const isEnded = function (player, flv) {
   if (flv.videoDuration - player.currentTime * flv.videoTimeScale < 2 * flv.videoTimeScale) {
-    const range = player.getBufferedRange();
+    const range = player.getBufferedRange()
     if (player.currentTime - range[1] < 0.1) {
-      player.mse.endOfStream();
+      player.mse.endOfStream()
     }
   }
-};
-let tempCurrentTime = 0;
+}
+let tempCurrentTime = 0
 const flvPlayer = function () {
-  let player = this,
-    flv,
-    mse,
-    seekDataReceived,
-    requestSending = false,
-    _config = Object.assign(defaultConfig, player.config),
-    sniffer = Player.sniffer;
-  const { preloadTime, minCachedTime, isLive } = _config;
+  let player = this
+  let flv
+  let mse
+  let seekDataReceived
+  let requestSending = false
+  let _config = Object.assign(defaultConfig, player.config)
+  // let sniffer = Player.sniffer
+  const { preloadTime, minCachedTime, isLive } = _config
 
-  function progressChecker() {
-    const range = player.getBufferedRange();
-    if (flv.videoDuration - range[1] * flv.videoTimeScale < 0.1 * flv.videoTimeScale) { return; }
+  function progressChecker () {
+    const range = player.getBufferedRange()
+    if (flv.videoDuration - range[1] * flv.videoTimeScale < 0.1 * flv.videoTimeScale) { return }
     if (range[1] - player.currentTime < minCachedTime && !requestSending) {
-      requestSending = true;
+      requestSending = true
       flv.loadSegments(true, player.currentTime, preloadTime).then(() => {
-        requestSending = false;
-      });
+        requestSending = false
+      })
     }
   }
 
-  const _start = player.start;
+  const _start = player.start
   player.start = function (url = _config.url) {
-    if (!url) { return; }
-    flv = new MainParser(_config, player);
+    if (!url) { return }
+    flv = new MainParser(_config, player)
 
-    flv.on('ready', (ftyp_moov) => {
-      mse = player.mse = flv.mse = new MSE();
-      _start.call(player, mse.url);
+    flv.on('ready', (ftypMoov) => {
+      mse = player.mse = flv.mse = new MSE()
+      _start.call(player, mse.url)
 
       mse.on('sourceopen', () => {
-        flv.isSourceOpen = true;
-        mse.appendBuffer(ftyp_moov.buffer);
+        flv.isSourceOpen = true
+        mse.appendBuffer(ftypMoov.buffer)
         mse.on('updateend', () => {
-          const { pendingFragments, hasPendingFragments } = flv;
+          const { pendingFragments, hasPendingFragments } = flv
 
-          seekDataReceived = true;
+          seekDataReceived = true
           if (hasPendingFragments) {
-            const fragment = pendingFragments.shift();
+            const fragment = pendingFragments.shift()
             if (!mse.appendBuffer(fragment.data)) {
-              pendingFragments.unshift(fragment);
+              pendingFragments.unshift(fragment)
             } else {
-              player.emit('cacheupdate', player);
+              player.emit('cacheupdate', player)
             }
           }
-        });
-      });
+        })
+      })
 
       mse.on('error', (e) => {
-        player.emit('error', e);
-      });
-    });
+        player.emit('error', e)
+      })
+    })
 
     flv.handleError = function (e) {
-      player.emit('error', e);
-    };
-  };
+      player.emit('error', e)
+    }
+  }
 
   player.on('pause', () => {
-    !isLive && VodTask.clear();
-  });
+    !isLive && VodTask.clear()
+  })
   //
   // player.on('resume', function () {
   //
@@ -83,67 +83,66 @@ const flvPlayer = function () {
   //     console.log('waiting');
   // });
 
-
   if (!isLive) {
     player.on('seeking', () => {
-      VodTask.clear();
-      const { buffered, currentTime } = player;
-      let isBuffered = false;
+      VodTask.clear()
+      const { buffered, currentTime } = player
+      let isBuffered = false
       if (buffered.length) {
         for (let i = 0, len = buffered.length; i < len; i++) {
           if (currentTime > buffered.start(i) && currentTime < buffered.end(i)) {
-            isBuffered = true;
-            break;
+            isBuffered = true
+            break
           }
         }
       }
       if (isBuffered) {
-        return;
+        return
       }
       if (!flv.isSeekable) {
-        player.currentTime = tempCurrentTime;
-        return;
+        player.currentTime = tempCurrentTime
+        return
       }
-      flv.seek(player.currentTime, preloadTime, player);
-      seekDataReceived = false;
-    });
+      flv.seek(player.currentTime, preloadTime, player)
+      seekDataReceived = false
+    })
     player.on('timeupdate', () => {
-      tempCurrentTime = player.currentTime;
+      tempCurrentTime = player.currentTime
       if (seekDataReceived !== false) {
-        progressChecker();
+        progressChecker()
       }
-      isEnded(player, flv);
-    });
-    player.beforeReplay = function () {
-      VodTask.clear();
-      const mse = flv.mse = new MSE();
-      flv.replay();
+      isEnded(player, flv)
+    })
+    player._replay = function () {
+      VodTask.clear()
+      const mse = flv.mse = new MSE()
+      flv.replay()
       mse.on('sourceopen', () => {
-        flv.isSourceOpen = true;
-        mse.appendBuffer(flv.ftyp_moof.buffer);
+        flv.isSourceOpen = true
+        mse.appendBuffer(flv.ftyp_moof.buffer)
         mse.on('updateend', () => {
-          const { pendingFragments, hasPendingFragments } = flv;
-          seekDataReceived = true;
+          const { pendingFragments, hasPendingFragments } = flv
+          seekDataReceived = true
           if (hasPendingFragments) {
-            const fragment = pendingFragments.shift();
+            const fragment = pendingFragments.shift()
             if (!mse.appendBuffer(fragment.data)) {
-              pendingFragments.unshift(fragment);
+              pendingFragments.unshift(fragment)
             } else {
-              player.emit('cacheupdate', player);
+              player.emit('cacheupdate', player)
             }
           }
-        });
-      });
+        })
+      })
 
       mse.on('error', (e) => {
-        player.emit('error', e);
-      });
-      player.emit('cacheupdate', player);
-      player.src = mse.url;
-      player.mse = mse;
-      return true;
-    };
+        player.emit('error', e)
+      })
+      player.emit('cacheupdate', player)
+      player.src = mse.url
+      player.mse = mse
+      return true
+    }
   }
-};
+}
 
-Player.install('flvplayer', flvPlayer);
+Player.install('flvplayer', flvPlayer)
