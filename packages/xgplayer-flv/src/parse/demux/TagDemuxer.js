eb97ca54bf99fffa071540203a193e5283727092
leonardoFu <yuhao.fu1995@gmail.com>
2018-07-10 18:42:32 +0800

    feat(flv/[index.js, Flv.js, MainParser.js]): flv support seamless definition switching, see configur
    provide difinition change api for flv and flv.js

--- packages/xgplayer-flv/src/parse/demux/TagDemuxer.js
+++ packages/xgplayer-flv/src/parse/demux/TagDemuxer.js
@@ -1,126 +1,129 @@
-import Demuxer from './Demuxer';
-import MetaDemuxer from './MetaDemuxer';
-import VideoDemuxer from './VideoDemuxer';
-import AudioDemuxer from './AudioDemuxer';
-import Logger from '../../utils/Log';
-import metaFields from '../../constants/metaFields';
-import { EventTypes } from '../../constants/types';
+import Demuxer from './Demuxer'
+import MetaDemuxer from './MetaDemuxer'
+import VideoDemuxer from './VideoDemuxer'
+import AudioDemuxer from './AudioDemuxer'
+import Logger from '../../utils/Log'
+import metaFields from '../../constants/metaFields'
+import {EventTypes} from '../../constants/types'
 
-const nativeHasProp = Object.prototype.hasOwnProperty;
+const nativeHasProp = Object.prototype.hasOwnProperty
 
 export default class Tagdemux extends Demuxer {
+  constructor (store) {
+    super(store)
+    this.CLASS_NAME = this.constructor.name
+    this._metaDemuxer = new MetaDemuxer(store)
+    this._videoDemuxer = new VideoDemuxer(store)
+    this._audioDemuxer = new AudioDemuxer(store)
+    this._firstParse = true
+    this._dataOffset = 0
+  }
 
-    constructor (store) {
-        super(store);
-        this.CLASS_NAME = this.constructor.name;
-        this._metaDemuxer = new MetaDemuxer(store);
-        this._videoDemuxer = new VideoDemuxer(store);
-        this._audioDemuxer = new AudioDemuxer(store);
-        this._firstParse = true;
-        this._dataOffset = 0;
+  destroy () {
+    this._metaDemuxer = null
+    this._videoDemuxer = null
+    this._audioDemuxer = null
+  }
 
+  resolveTags () {
+    const {tags} = this._store.state
+
+    const {_store: store} = this
+    const {videoTrack, audioTrack} = store
+
+    tags.forEach((tag) => {
+      this.resolveTag(tag)
+    })
+
+    if (this._store.hasInitialMetaDispatched) {
+      if (videoTrack.length || audioTrack.length) {
+        this.dispatch(EventTypes.DATA_READY, audioTrack, videoTrack)
+      }
     }
 
-    resolveTags () {
-        const { tags } = this._store.state;
+    this._store.state.tags = []
+  }
 
-        const { _store: store } = this;
-        const { videoTrack, audioTrack } = store;
+  resolveTag (tag) {
+    switch (String(tag.tagType)) {
+      case '8': // audio
+        this._resolveAudioTag(tag)
+        break
+      case '9': // video
+        this._resolveVideoTag(tag)
+        break
+      case '18': // metadata
+        this._resolveMetaTag(tag)
+        break
+    }
+  }
 
-        tags.forEach((tag) => {
-            this.resolveTag(tag);
-        });
+  _resolveAudioTag (tag) {
+    if (tag.bodySize <= 1) {
+      this.warn('Not enough data for audio tag body')
+    }
+    this._audioDemuxer.resolve(tag)
+  }
 
-        if (this._store.hasInitialMetaDispatched) {
-            if (videoTrack.length || audioTrack.length) {
-                this.dispatch(EventTypes.DATA_READY, audioTrack, videoTrack);
-            }
+  _resolveVideoTag (tag) {
+    if (tag.bodySize <= 1) {
+      this.error('Not enough data for video tag body')
+      return
+    }
+    const {_hasVideo, hasVideoFlagOverrided} = this
+    if (hasVideoFlagOverrided && !_hasVideo) {
+      return
+    }
+
+    this._videoDemuxer.resolve(tag)
+  }
+
+  _initMetaData (metaData) {
+    const {_store: s} = this
+    if (nativeHasProp.call(metaData, 'onMetaData')) {
+      if (s.hasMetaData) {
+        Logger.log(`[${this.CLASS_NAME}]`, 'found another meta tag')
+      }
+      s.metaData = metaData
+      const onMetaData = metaData.onMetaData
+
+      metaFields.forEach(field => {
+        const {name, type, parser, onTypeErr} = field
+        if (Object(onMetaData[name]) instanceof type) {
+          parser.call(this, s, onMetaData)
+        } else {
+          if (onTypeErr && onTypeErr instanceof Function) {
+            onTypeErr(s, onMetaData)
+          }
         }
+      })
 
-        this._store.state.tags = [];
+      this._store.mediaInfo._metaData = metaData
+      // 同步到共享store
+      if (this._store.mediaInfo.isComplete) {
+        this.dispatch(EventTypes.MEDIA_INFO_READY, this._store.mediaInfo)
+      }
+    }
+  }
+
+  _resolveMetaTag (tag) {
+    const {body} = tag
+    const metaObj = this._metaDemuxer.resolve(body, body.length)
+    this._initMetaData(metaObj)
+  }
+
+  _parseKeyframes (keyframes) {
+    let times = []
+    let filePositions = []
+    const {videoTimeScale, state} = this._store
+    for (let i = 1; i < keyframes.times.length; i++) {
+      times[times.length] = state.timeStampBase + Math.floor(keyframes.times[i] * videoTimeScale)
+      filePositions[filePositions.length] = keyframes.filepositions[i]
     }
 
-    resolveTag (tag) {
-
-        switch (String(tag.tagType)) {
-            case '8': // audio
-                this._resolveAudioTag(tag);
-                break;
-            case '9': // video
-                this._resolveVideoTag(tag);
-                break;
-            case '18': // metadata
-                this._resolveMetaTag(tag);
-                break;
-        }
-
-
+    return {
+      times,
+      filePositions
     }
-
-    _resolveAudioTag (tag) {
-        if (tag.bodySize <= 1) {
-            this.warn('Not enough data for audio tag body');
-        }
-        this._audioDemuxer.resolve(tag);
-    }
-    _resolveVideoTag (tag) {
-        if (tag.bodySize <= 1) {
-            this.error('Not enough data for video tag body');
-            return;
-        }
-        const { _hasVideo, hasVideoFlagOverrided } = this;
-        if (hasVideoFlagOverrided && !_hasVideo) {
-            return;
-        }
-
-        this._videoDemuxer.resolve(tag);
-    }
-    _initMetaData (metaData) {
-        const { _store: s } = this;
-        if (nativeHasProp.call(metaData, 'onMetaData')) {
-            if (s.hasMetaData) {
-                Logger.log(`[${this.CLASS_NAME}]`, 'found another meta tag');
-            }
-            s.metaData = metaData;
-            const onMetaData = metaData.onMetaData;
-
-            metaFields.forEach(field => {
-                const { name, type, parser, onTypeErr } = field;
-                if (Object(onMetaData[name]) instanceof type) {
-                    parser.call(this, s, onMetaData);
-                } else {
-                    if (onTypeErr && onTypeErr instanceof Function) {
-                        onTypeErr(s, onMetaData);
-                    }
-                }
-            });
-
-            this._store.mediaInfo._metaData = metaData;
-            // 同步到共享store
-            if (this._store.mediaInfo.isComplete) {
-                this.dispatch(EventTypes.MEDIA_INFO_READY, this._store.mediaInfo);
-            }
-        }
-    }
-
-    _resolveMetaTag (tag) {
-        const { body } = tag;
-        const metaObj = this._metaDemuxer.resolve(body, body.length);
-        this._initMetaData(metaObj);
-    }
-
-    _parseKeyframes (keyframes) {
-        let times = [], filePositions = [];
-        const { videoTimeScale, state } = this._store;
-        for (let i = 1; i < keyframes.times.length; i++) {
-            times[times.length] = state.timeStampBase + Math.floor(keyframes.times[i] * videoTimeScale);
-            filePositions[filePositions.length] = keyframes.filepositions[i];
-        }
-
-        return {
-            times,
-            filePositions,
-        };
-    }
-
+  }
 }
