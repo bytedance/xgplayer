fe22152a680ae4a86440014d6750221a8b7977ed
张鑫 <zhangxin.frontend@bytedance.com>
2018-08-03 15:26:28 +0800

    fix(xgplayer/[control/error.js,style/player.scss]): fix error info suitability problem when player w

--- packages/xgplayer/src/control/error.js
+++ packages/xgplayer/src/control/error.js
@@ -1,17 +1,26 @@
 import Player from '../player'
 
 let error = function () {
-  let player = this; let util = Player.util; let error = util.createDom('xg-error', '<em class="xgplayer-error-text"></em>，请<span class="xgplayer-error-refresh">刷新</span>试试 ', {}, 'xgplayer-error')
+  let player = this; let util = Player.util; let error = util.createDom('xg-error', '<em class="xgplayer-error-text">请<span class="xgplayer-error-refresh">刷新</span>试试</em>', {}, 'xgplayer-error')
   player.root.appendChild(error)
   let text = error.querySelector('.xgplayer-error-text')
   let refresh = error.querySelector('.xgplayer-error-refresh')
+
   player.on('error', function () {
-    text.textContent = player.lang[player.video.error.message] || player.lang.ERROR
+    player.controls.style.display = 'none'
+    if(player.config.lang && player.config.lang === "zh-cn") {
+      text.innerHTML = `${player.lang[player.video.error.message] || player.lang.ERROR}，请<span class="xgplayer-error-refresh">刷新</span>试试`
+    } else {
+      text.innerHTML = `${player.lang[player.video.error.message] || player.lang.ERROR}，please try to <span class="xgplayer-error-refresh">refresh</span>`
+    }
+
     util.addClass(player.root, 'xgplayer-is-error')
+    refresh = error.querySelector('.xgplayer-error-refresh')
   });
 
   ['touchstart', 'click'].forEach(item => {
     refresh.addEventListener(item, function (e) {
+      player.controls.style.display = 'flex'
       player.reload()
     })
   })
