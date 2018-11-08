4430c6d6887e6ba8804376b4302ee61933243057
张鑫 <zhangxin.frontend@bytedance.com>
2018-11-08 10:19:26 +0800

    feat(xgplayer, xgplayer-music): add img config for controls(play, pause, next, prev, forward, backwa

--- packages/xgplayer-music/src/controls/backward.js
+++ packages/xgplayer-music/src/controls/backward.js
@@ -4,10 +4,16 @@ const backward = (player) => {
   const util = Player.util
   const controlEl = player.controls
   const ev = ['click', 'touchstart']
-  const backward = util.createDom('xg-backward', `<xg-icon class="xgplayer-icon"><svg xmlns="http://www.w3.org/2000/svg" width="1024" height="1024" viewBox="0 0 1024 1024">
-            <path transform = "scale(1.5 1.5) translate(8 4.5)"
-            d="m 14,2.99996 0,10 -7,-5 7,-5 z m -7,5 0,5 -7,-5 7,-5 0,5 z m -7,0 0,0 z"></path>
-        </svg></xg-icon>`, {}, 'xgplayer-backward')
+  let backwardBtn = player.config.backwardBtn ? player.config.backwardBtn : {}
+  let backward
+  if (backwardBtn.type === 'img') {
+    backward = Player.util.createImgBtn('backward', backwardBtn.url, backwardBtn.width, backwardBtn.height)
+  } else {
+    backward = util.createDom('xg-backward', `<xg-icon class="xgplayer-icon"><svg xmlns="http://www.w3.org/2000/svg" width="1024" height="1024" viewBox="0 0 1024 1024">
+              <path transform = "scale(1.5 1.5) translate(8 4.5)"
+              d="m 14,2.99996 0,10 -7,-5 7,-5 z m -7,5 0,5 -7,-5 7,-5 0,5 z m -7,0 0,0 z"></path>
+          </svg></xg-icon>`, {}, 'xgplayer-backward')
+  }
   controlEl.appendChild(backward)
   ev.forEach(item => {
     backward.addEventListener(item, function (e) {
--- packages/xgplayer-music/src/controls/next.js
+++ packages/xgplayer-music/src/controls/next.js
@@ -4,10 +4,16 @@ const next = (player) => {
   const util = Player.util
   const controlEl = player.controls
   const ev = ['click', 'touchstart']
-  const next = util.createDom('xg-next', `<xg-icon class="xgplayer-icon"><svg xmlns="http://www.w3.org/2000/svg" width="1024" height="1024" viewBox="0 0 1024 1024">
-            <path transform="scale(0.025 0.025)"
-            d="M800 380v768h-128v-352l-320 320v-704l320 320v-352z"></path>
-        </svg></xg-icon>`, {}, 'xgplayer-next')
+  let nextBtn = player.config.nextBtn ? player.config.nextBtn : {}
+  let next
+  if (nextBtn.type === 'img') {
+    next = Player.util.createImgBtn('next', nextBtn.url, nextBtn.width, nextBtn.height)
+  } else {
+    next = util.createDom('xg-next', `<xg-icon class="xgplayer-icon"><svg xmlns="http://www.w3.org/2000/svg" width="1024" height="1024" viewBox="0 0 1024 1024">
+              <path transform="scale(0.025 0.025)"
+              d="M800 380v768h-128v-352l-320 320v-704l320 320v-352z"></path>
+          </svg></xg-icon>`, {}, 'xgplayer-next')
+  }
   controlEl.appendChild(next)
   ev.forEach(item => {
     next.addEventListener(item, function (e) {
