0016f2006cb4bdeb1fb20dc9205c4ee5711e3e0f
Mayde <maysjtu@163.com>
2018-12-01 17:11:55 +0800

    add: 为播放器添加rotate方法，方便直接调用

--- packages/xgplayer/src/control/rotate.js
+++ packages/xgplayer/src/control/rotate.js
@@ -8,26 +8,29 @@ const rotate = function () {
   let root = player.root
   root.appendChild(btn)
   let rotateDeg = 0
-  btn.addEventListener('click', function () {
-    let width = player.root.offsetWidth
-    let height = player.root.offsetHeight
-    player.root.style.width = height + 'px'
-    player.root.style.height = width + 'px'
-    rotateDeg = (rotateDeg + 0.25) % 1
-    let scale
-    if (rotateDeg === 0.25 || rotateDeg === 0.75) {
-      if (width >= height) {
-        scale = (width / height).toFixed(2)
+  if (!player.rotate) {
+    player.rotate = function () {
+      let width = player.root.offsetWidth
+      let height = player.root.offsetHeight
+      player.root.style.width = height + 'px'
+      player.root.style.height = width + 'px'
+      rotateDeg = (rotateDeg + 0.25) % 1
+      let scale
+      if (rotateDeg === 0.25 || rotateDeg === 0.75) {
+        if (width >= height) {
+          scale = (width / height).toFixed(2)
+        } else {
+          scale = (height / width).toFixed(2)
+        }
       } else {
-        scale = (height / width).toFixed(2)
+        scale = 1
       }
-    } else {
-      scale = 1
+      player.video.style.transform = `rotate(${rotateDeg}turn) scale(${scale})`
+      player.video.style.webKitTransform = `rotate(${rotateDeg}turn) scale(${scale})`
+      player.emit('rotate', rotateDeg * 360)
     }
-    player.video.style.transform = `rotate(${rotateDeg}turn) scale(${scale})`
-    player.video.style.webKitTransform = `rotate(${rotateDeg}turn) scale(${scale})`
-    player.emit('rotate', rotateDeg * 360)
-  })
+  }
+  btn.addEventListener('click', player.rotate)
 }
 
 Player.install('rotate', rotate)
