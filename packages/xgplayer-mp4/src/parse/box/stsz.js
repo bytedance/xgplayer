fc9436ac041cf2b7ea31b7b6e545790ca17b8915
Mayde <maysjtu@163.com>
2018-12-12 15:43:20 +0800

    fix: fix stsz box

--- packages/xgplayer-mp4/src/parse/box/stsz.js
+++ packages/xgplayer-mp4/src/parse/box/stsz.js
@@ -7,10 +7,12 @@ Box.stsz = function () {
   this.flag = Stream.readByte(stream.dataview, 3)
   this.sampleSize = stream.readUint32()
   this.count = stream.readUint32()
-  let entries = []
-  this.entries = entries
-  for (let i = 0, count = this.count; i < count; i++) {
-    entries.push(stream.readUint32())
+  if (this.sampleSize === 0) {
+    let entries = []
+    this.entries = entries
+    for (let i = 0, count = this.count; i < count; i++) {
+      entries.push(stream.readUint32())
+    }
   }
   delete this.subBox
   delete this.data
