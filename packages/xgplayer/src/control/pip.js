193cdcdd4d71c9f466e7f93bf7134ca102e3438e
bethe-light <1598204060@qq.com>
2019-03-20 15:16:52 +0800

    add feature open pipApi

--- packages/xgplayer/src/control/pip.js
+++ packages/xgplayer/src/control/pip.js
@@ -1,5 +1,4 @@
 import Player from '../player'
-import Draggabilly from 'draggabilly'
 
 let pip = function () {
   let player = this
@@ -8,55 +7,18 @@ let pip = function () {
     return
   }
   let btn = util.createDom('xg-pip', '<p class="name"><span>画中画</span></p>', {tabindex: 9}, 'xgplayer-pip')
-  let dragLay = util.createDom('xg-pip-lay', '<div></div>', {}, 'xgplayer-pip-lay')
-  let dragHandle = util.createDom('xg-pip-drag', '<div class="drag-handle"><span>点击按住可拖动视频</span></div>', {tabindex: 9}, 'xgplayer-pip-drag')
   let root = player.controls
   let container = player.root
   root.appendChild(btn)
-  container.appendChild(dragHandle)
-  container.appendChild(dragLay)
-  let draggie = new Draggabilly('.xgplayer', {
-    handle: '.drag-handle'
-  })
-
-  let getPIP = function (el) {
-    let ro = player.root.getBoundingClientRect()
-    let Top = ro.top
-    let Left = ro.left
-
-    util.addClass(el, 'xgplayer-pip-active')
-    player.root.style.right = 0
-    player.root.style.bottom = '200px'
-    player.root.style.top = ''
-    player.root.style.left = ''
-    if (player.config.fluid) {
-      player.root.style['padding-top'] = ''
-    }
-    ['click', 'touchstart'].forEach(item => {
-      dragLay.addEventListener(item, function (e) {
-        e.preventDefault()
-        e.stopPropagation()
-        exitPIP(container)
-        player.root.style.top = `${Top}px`
-        player.root.style.left = `${Left}px`
-      })
-    })
-  }
-  let exitPIP = function (el) {
-    util.removeClass(el, 'xgplayer-pip-active')
-    if (player.config.fluid) {
-      player.root.style['padding-top'] = `${player.config.height * 100 / player.config.width}%`
-    }
-  };
-
-  ['click', 'touchstart'].forEach(item => {
+  let clickEvents = ['click', 'touchstart']
+  clickEvents.forEach(item => {
     btn.addEventListener(item, function (e) {
       e.preventDefault()
       e.stopPropagation()
       if (util.hasClass(container, 'xgplayer-pip-active')) {
-        exitPIP(container)
+        player.exitPIP(player)
       } else {
-        getPIP(container)
+        player.getPIP(player)
       }
     })
   })
