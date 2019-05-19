632c794530b7ec3fe277b0cb529fb77fd3860f26
zhangxin.frontend <zhangxin.frontend@bytedance.com>
2019-05-19 16:54:57 +0800

    feat(xgplayer): add execBeforePluginsCall; publish 1.1.6-beta.2

--- packages/xgplayer/src/player.js
+++ packages/xgplayer/src/player.js
@@ -61,6 +61,9 @@ class Player extends Proxy {
       this.root.style.width = `${this.config.width}px`
       this.root.style.height = `${this.config.height}px`
     }
+    this.config.execBeforePluginsCall.forEach(item => {
+      item.call(this, this)
+    })
     if (this.config.controlStyle && util.typeOf(this.config.controlStyle) === 'String') {
       let self = this
       fetch(self.config.controlStyle, {
