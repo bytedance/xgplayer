7a65c683319b73ec38d14f93dbb615b1de063626
zhangxin.frontend <zhangxin.frontend@bytedance.com>
2019-07-12 21:29:23 +0800

    fix(xgplayer): add pip config; support player string size

--- packages/xgplayer/src/player.js
+++ packages/xgplayer/src/player.js
@@ -61,8 +61,22 @@ class Player extends Proxy {
       this.video.style['top'] = '0'
       this.video.style['left'] = '0'
     } else {
-      this.root.style.width = `${this.config.width}px`
-      this.root.style.height = `${this.config.height}px`
+      // this.root.style.width = `${this.config.width}px`
+      // this.root.style.height = `${this.config.height}px`
+      if (this.config.width) {
+        if(typeof this.config.width !== 'number') {
+          this.root.style.width = this.config.width
+        } else {
+          this.root.style.width = `${this.config.width}px`
+        }
+      }
+      if (this.config.height) {
+        if(typeof this.config.height !== 'number') {
+          this.root.style.height = this.config.height
+        } else {
+          this.root.style.height = `${this.config.height}px`
+        }
+      }
     }
     if(this.config.execBeforePluginsCall) {
       this.config.execBeforePluginsCall.forEach(item => {
@@ -359,21 +373,12 @@ class Player extends Proxy {
   }
 
   getPIP () {
-    let ro = this.root.getBoundingClientRect()
-    let Top = ro.top
-    let Left = ro.left
+    // let ro = this.root.getBoundingClientRect()
+    // let Top = ro.top
+    // let Left = ro.left
     let dragLay = util.createDom('xg-pip-lay', '<div></div>', {}, 'xgplayer-pip-lay')
     this.root.appendChild(dragLay)
-    let langType = this.config.lang
-    let dragTips
-    if(langType === 'en') {
-      dragTips = 'Tap and hold to drag the video'
-    } else if(langType === 'jp') {
-      dragTips = '長押ししてビデオをドラッグ'
-    } else {
-      dragTips = '点击按住可拖动视频'
-    }
-    let dragHandle = util.createDom('xg-pip-drag', `<div class="drag-handle"><span>${dragTips}</span></div>`, {tabindex: 9}, 'xgplayer-pip-drag')
+    let dragHandle = util.createDom('xg-pip-drag', '<div class="drag-handle"><span>点击按住可拖动视频</span></div>', {tabindex: 9}, 'xgplayer-pip-drag')
     this.root.appendChild(dragHandle)
     let draggie = new Draggabilly('.xgplayer', {
       handle: '.drag-handle'
@@ -383,17 +388,41 @@ class Player extends Proxy {
     this.root.style.bottom = '200px'
     this.root.style.top = ''
     this.root.style.left = ''
+    this.root.style.width = '320px'
+    this.root.style.height = '180px'
+    if(this.config.pipConfig) {
+      if(this.config.pipConfig.top !== undefined) {
+        this.root.style.top = this.config.pipConfig.top + 'px'
+        this.root.style.bottom = ''
+      }
+      if(this.config.pipConfig.bottom !== undefined) {
+        this.root.style.bottom = this.config.pipConfig.bottom + 'px'
+      }
+      if(this.config.pipConfig.left !== undefined) {
+        this.root.style.left = this.config.pipConfig.left + 'px'
+        this.root.style.right = ''
+      }
+      if(this.config.pipConfig.right !== undefined) {
+        this.root.style.right = this.config.pipConfig.right + 'px'
+      }
+      if(this.config.pipConfig.width !== undefined) {
+        this.root.style.width = this.config.pipConfig.width + 'px'
+      }
+      if(this.config.pipConfig.height !== undefined) {
+        this.root.style.height = this.config.pipConfig.height + 'px'
+      }
+    }
     if (this.config.fluid) {
       this.root.style['padding-top'] = ''
     }
     let player = this;
-    ['click', 'touchstart'].forEach(item => {
+    ['click', 'touchend'].forEach(item => {
       dragLay.addEventListener(item, function (e) {
         e.preventDefault()
         e.stopPropagation()
         player.exitPIP()
-        player.root.style.top = `${Top}px`
-        player.root.style.left = `${Left}px`
+        // player.root.style.top = `${Top}px`
+        // player.root.style.left = `${Left}px`
       })
     })
   }
@@ -405,7 +434,33 @@ class Player extends Proxy {
     this.root.style.top = ''
     this.root.style.left = ''
     if (this.config.fluid) {
+      this.root.style['width'] = '100%'
+      this.root.style['height'] = '0'
       this.root.style['padding-top'] = `${this.config.height * 100 / this.config.width}%`
+    } else {
+      if (this.config.width) {
+        if(typeof this.config.width !== 'number') {
+          this.root.style.width = this.config.width
+        } else {
+          this.root.style.width = `${this.config.width}px`
+        }
+      }
+      if (this.config.height) {
+        if(typeof this.config.height !== 'number') {
+          this.root.style.height = this.config.height
+        } else {
+          this.root.style.height = `${this.config.height}px`
+        }
+      }
+    }
+
+    let dragLay = util.findDom(this.root, '.xgplayer-pip-lay')
+    if(dragLay && dragLay.parentNode) {
+      dragLay.parentNode.removeChild(dragLay)
+    }
+    let dragHandle = util.findDom(this.root, '.xgplayer-pip-drag')
+    if(dragHandle && dragHandle.parentNode) {
+      dragHandle.parentNode.removeChild(dragHandle)
     }
   }
 
