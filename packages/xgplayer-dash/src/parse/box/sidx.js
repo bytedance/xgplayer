be410deb0251b0766ad90be7cbb235e117b7bd78
divawth <752990270@qq.com>
2019-08-09 14:16:35 +0800

    feature dash segmentbase

---
+++ packages/xgplayer-dash/src/parse/box/sidx.js
@@ -0,0 +1,38 @@
+import Box from '../box'
+import Stream from '../stream'
+
+Box.sidx = function () {
+  let stream = new Stream(this.data)
+  this.version = stream.readUint8()
+  this.flag = Stream.readByte(stream.dataview, 3)
+
+  this.reference_ID = stream.readUint32();
+	this.timescale = stream.readUint32();
+	if (this.version === 0) {
+		this.earliest_presentation_time = stream.readUint32();
+		this.first_offset = stream.readUint32();
+	} else {
+		this.earliest_presentation_time = stream.readUint64();
+		this.first_offset = stream.readUint64();
+	}
+	stream.readUint16();
+	this.references = [];
+	let count = stream.readUint16();
+	for (let i = 0; i < count; i++) {
+		let ref = {};
+		this.references.push(ref);
+		let tmp_32 = stream.readUint32();
+		ref.reference_type = (tmp_32 >> 31) & 0x1;
+		ref.referenced_size = tmp_32 & 0x7FFFFFFF;
+		ref.subsegment_duration = stream.readUint32();
+		tmp_32 = stream.readUint32();
+		ref.starts_with_SAP = (tmp_32 >> 31) & 0x1;
+		ref.SAP_type = (tmp_32 >> 28) & 0x7;
+		ref.SAP_delta_time = tmp_32 & 0xFFFFFFF;
+	}
+	// this.first_offset += stream.position;
+
+  delete this.subBox
+  delete this.data
+  stream = null
+}
