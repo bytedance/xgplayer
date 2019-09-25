dbb7e026887e82efdf917fea23643bb30933b1dd
leonardoFu <yuhao.fu1995@gmail.com>
2019-09-26 01:14:32 +0800

    feat: video的兼容性基本开发完成，需要调整算法，不处理丢帧

--- packages/xgplayer-codec/src/compatibility.js
+++ packages/xgplayer-codec/src/compatibility.js
@@ -6,6 +6,8 @@ const {REMUX_EVENTS} = EVENTS
 class Compatibility {
   constructor () {
     this.nextAudioDts = 0
+    this.nextVideoDts = 0
+
     this._firstAudioSample = null
     this._firstVideoSample = null
   }
@@ -49,31 +51,99 @@ class Compatibility {
 
     // step0. 修复视频首帧为非关键帧的问题
     if (first) {
-      const firstSampleIdx = firstSample ? audioSamples.indexOf(firstSample) : 0
+      const firstSampleIdx = firstSample ? videoSamples.indexOf(firstSample) : 0
 
       if (firstSampleIdx) {
-        audioSamples.splice(0, firstSampleIdx)
+        videoSamples.splice(0, firstSampleIdx)
       }
     }
 
     // step1. 修复与audio首帧差距太大的问题
     if (this._firstAudioSample) {
-      const gap = firstDts - this._firstAudioSample.dts
+      const gap = firstDts - audioFirstDts
       if (gap > meta.refSampleDuration) {
-        const clonedFirstSample = Object.assign({}, firstSample) // 视频头部帧缺失需要复制第一帧
         const fillCount = gap / meta.refSampleDuration
 
         for (let i = 0; i < fillCount; i++) {
+          const clonedFirstSample = Object.assign({}, firstSample) // 视频头部帧缺失需要复制第一帧
           // 重新计算sample的dts和pts
           clonedFirstSample.dts = i * meta.refSampleDuration
           clonedFirstSample.pts = clonedFirstSample.dts + clonedFirstSample.cts
 
+          videoSamples.unshift(clonedFirstSample)
         }
-
       }
     }
 
+    let gap
+    // step2. 修复samples段之间的间距问题、
+    if (this.nextVideoDts) {
+      // step1. 处理samples段之间的丢帧情况
+      // 当发现duration差距大于2帧时进行补帧
+      gap = firstDts - this.nextVideoDts
+      if (gap > meta.refSampleDuration) {
+        const fillFrameCount = Math.floor(gap / meta.refSampleDuration)
+        const clonedSample = Object.assign({}, videoSamples[0])
 
+        for (let i = 0; i < fillFrameCount; i++) {
+          clonedSample.dts = this.nextVideoDts + i * meta.refSampleDuration
+          clonedSample.pts = clonedSample.dts + clonedSample.cts
+
+          this.videoTrack.samples.unshift(clonedSample)
+        }
+      } else if (Math.abs(gap) < meta.refSampleDuration) {
+        // 当差距在+-一帧之间时将第一帧的dts强行定位到期望位置
+        videoSamples[0].dts = this.nextVideoDts
+        videoSamples[0].pts = videoSamples[0].dts + videoSamples[0].cts
+      } else if (gap < -1 * meta.refSampleDuration) {
+        // 发生了dts的倒退，丢帧
+        videoSamples.shift()
+      }
+    }
+
+    // step2. 修复sample段之内的间距问题
+    // step3. 修复samples段内部的dts异常问题
+    const droppedIdxArr = []
+    let current = null// 当前用于比较的基准帧
+    for (let i = 0, len = videoSamples.length; i < len; i++) {
+      if (droppedIdxArr.indexOf(i) < 0 && !current) {
+        // 对于将要丢弃的帧，不进行比较
+        current = videoSamples[i]
+      }
+      const next = videoSamples[i + 1]
+
+      if (!next) {
+        break;
+      }
+
+      const duration = next.dts - current.dts;
+
+      if (duration > meta.refSampleDuration) {
+        // 两帧之间间隔太大，需要补空白帧
+        let fillFrameCount = Math.floor(duration / meta.refSampleDuration)
+
+        let fillFrameIdx = 0
+        while (fillFrameIdx < fillFrameCount) {
+          const fillFrame = Object.assign({}, videoSamples[i] + 1)
+          fillFrame.dts = current.dts + fillFrameIdx * meta.refSampleDuration
+          fillFrame.pts = fillFrame.dts + fillFrame.cts
+          fillFrameIdx++
+          videoSamples.splice(i, 0, fillFrame)
+        }
+      } else if (duration < 0) {
+        // 将下一帧标记为丢弃
+        this.videoTrack.droppedSamples.push({
+          dts: next.dts,
+          datasize: next.datasize
+        })
+
+        droppedIdxArr.push(i + 1)
+      } else {
+        current = null
+      }
+    }
+
+    this.videoTrack.samples = videoSamples;
   }
 
   doFixAudio (first) {
@@ -106,7 +176,7 @@ class Compatibility {
       const videoFirstPts = this._firstVideoSample.pts ? this._firstVideoSample.pts : this._firstVideoSample.dts + this._firstVideoSample.cts
 
       if (firstSample.dts - videoFirstPts > meta.refSampleDuration) {
-        const silentSampleCount = (firstSample.dts - videoFirstPts) / meta.refSampleDuration
+        const silentSampleCount = Math.floor((firstSample.dts - videoFirstPts) / meta.refSampleDuration)
 
         for (let i = 0; i < silentSampleCount; i++) {
           const silentSample = {
@@ -156,12 +226,13 @@ class Compatibility {
 
     // step3. 修复samples段内部的dts异常问题
     const droppedIdxArr = []
-    let current = null// 当前用于比较的基准帧
+    let current = null; // 当前用于比较的基准帧
     for (let i = 0, len = audioSamples.length; i < len; i++) {
-      if (droppedIdxArr.indexOf(i) < 0 && !current) {
-        // 对于将要丢弃的帧，不进行比较
-        current = audioSamples[i]
+      if (droppedIdxArr.indexOf(i) > 0 || audioSamples[0]._isSilent) {
+        continue;
       }
+
+      current = audioSamples[i]
       const next = audioSamples[i + 1]
 
       if (!next) {
@@ -173,14 +244,18 @@ class Compatibility {
       if (duration > 2 * meta.refSampleDuration) {
         // 两帧之间间隔太大，需要补空白帧
         let silentFrameCount = Math.floor(duration / meta.refSampleDuration)
-        while (silentFrameCount > 0) {
-          silentFrameCount--
+        let frameIdx = 0
+
+        while (frameIdx < silentFrameCount) {
           const silentSample = {
             data: silentFrame,
             datasize: silentFrame.byteLength,
-            dts: current.dts + meta.refSampleDuration,
-            filtered: 0
+            dts: current.dts + (frameIdx + 1) * meta.refSampleDuration,
+            filtered: 0,
+            _isSilent: true
           }
+          frameIdx++
+
           audioSamples.splice(i, 0, silentSample)
         }
       } else if (duration < 0) {
