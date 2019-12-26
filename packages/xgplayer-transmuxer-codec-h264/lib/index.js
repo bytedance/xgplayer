56474840e20a5377297cc4f68c137369c076a96b
leonardoFu <yuhao.fu1995@gmail.com>
2019-12-26 09:44:34 +0800

    chore (transmuxer): add new dist file

---
+++ packages/xgplayer-transmuxer-codec-h264/lib/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.NalUnit = exports.SpsParser = undefined;
+
+var _sps = require('./sps');
+
+var _sps2 = _interopRequireDefault(_sps);
+
+var _nalunit = require('./nalunit');
+
+var _nalunit2 = _interopRequireDefault(_nalunit);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var SpsParser = exports.SpsParser = _sps2.default;
+var NalUnit = exports.NalUnit = _nalunit2.default;
---
+++ packages/xgplayer-utils/es/index.js
@@ -0,0 +1,18 @@
+import _Context from './Context';
+
+// Modules from env
+
+// Models
+import _MediaInfo from './models/media-info';
+import _MediaSample from './models/media-sample';
+import _MediaSegment from './models/media-segment';
+import _MediaSegmentList from './models/media-segment-list';
+
+import _debounce from './func/debounce';
+
+export var Context = _Context;
+export var debounce = _debounce;
+export var MediaInfo = _MediaInfo;
+export var MediaSample = _MediaSample;
+export var MediaSegment = _MediaSegment;
+export var MediaSegmentList = _MediaSegmentList;
