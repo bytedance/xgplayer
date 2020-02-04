9ffdd5d5fac002a1ef126aa8d41bde8a176a6276
涂强 <tuqiang.yuri@bytedance.com>
2020-02-04 17:31:35 +0800

    cl:rebuild

--- packages/xgplayer-render/browser/index.js
+++ packages/xgplayer-render/browser/index.js
@@ -93,34 +93,6 @@
 
   }
 
-<<<<<<< HEAD
-  var GLUtil =
-  /*#__PURE__*/
-  function () {
-    function GLUtil() {
-      _classCallCheck(this, GLUtil);
-    }
-
-    _createClass(GLUtil, null, [{
-      key: "createTexture",
-      value: function createTexture(gl, filter, data, width, height) {
-        var textureRef = gl.createTexture();
-        gl.bindTexture(gl.TEXTURE_2D, textureRef);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, filter);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, filter);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-
-        if (data instanceof Uint8Array) {
-          gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, data);
-        } else if (data instanceof HTMLVideoElement) {
-          gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, data);
-        } else if (data instanceof HTMLImageElement) {
-          gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, data);
-        } else if (data instanceof ImageData) {
-          gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, data);
-        }
-=======
   class Basic extends Filter {
     constructor(config) {
       super();
@@ -129,7 +101,6 @@
       this.opacity = config.opacity === undefined ? 1 : config.opacity;
       this.flip = config.flip;
     }
->>>>>>> new/live-mobile
 
     init(render) {
       this.rend = render;
@@ -496,86 +467,6 @@
 
       if (!this.outputTexuture) {
         this.outputTexuture = GLUtil.createTexture(gl, gl.LINEAR, new Uint8Array(width * height * 4), width, height);
-<<<<<<< HEAD
-        gl.bindFramebuffer(gl.FRAMEBUFFER, this.rend.fb);
-        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, this.outputTexuture, 0);
-        gl.useProgram(program);
-        gl.viewport(0, 0, this.canvas.width, this.canvas.height);
-        gl.activeTexture(gl.TEXTURE0);
-        gl.bindTexture(gl.TEXTURE_2D, yTextureRef);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width / 2, height / 2, 0, gl.RGBA, gl.UNSIGNED_BYTE, ydata);
-        gl.activeTexture(gl.TEXTURE1);
-        gl.bindTexture(gl.TEXTURE_2D, uTextureRef);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width / 2, height / 8, 0, gl.RGBA, gl.UNSIGNED_BYTE, udata);
-        gl.activeTexture(gl.TEXTURE2);
-        gl.bindTexture(gl.TEXTURE_2D, vTextureRef);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width / 2, height / 8, 0, gl.RGBA, gl.UNSIGNED_BYTE, vdata);
-        gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-        return this.outputTexuture;
-      }
-    }]);
-
-    return Yuv420;
-  }(Filter);
-
-  var Rgba =
-  /*#__PURE__*/
-  function (_Filter) {
-    _inherits(Rgba, _Filter);
-
-    function Rgba(config) {
-      var _this;
-
-      _classCallCheck(this, Rgba);
-
-      _this = _possibleConstructorReturn(this, _getPrototypeOf(Rgba).call(this));
-      _this.vShader = ['attribute vec4 vertexPos;', 'attribute vec2 texturePos;', 'varying vec2 textureCoord;', 'void main()', '{', '  gl_Position = vertexPos;', '  textureCoord = texturePos;', '}'].join('\n');
-      _this.fShader = ['precision highp float;', 'varying highp vec2 textureCoord;', 'uniform sampler2D sampler;', 'void main(void) {', '  vec4 color = texture2D(sampler, textureCoord);', '  gl_FragColor = vec4(color[0],color[1],color[2],color[3]);', '}'].join('\n');
-      return _this;
-    }
-
-    _createClass(Rgba, [{
-      key: "init",
-      value: function init(render) {
-        this.rend = render;
-        this.canvas = render.canvas;
-        var gl = this.gl = render.gl;
-        this.pw = GLUtil.createProgram(gl, this.vShader, this.fShader);
-        this.program = this.pw.program;
-        gl.useProgram(this.program); // vertexPos
-
-        var vertexPosBuffer = GLUtil.createBuffer(gl, new Float32Array([1, 1, -1, 1, 1, -1, -1, -1]));
-        GLUtil.bindAttribute(gl, vertexPosBuffer, this.pw.vertexPos, 2); // texturePos
-
-        this.texturePosBuffer = GLUtil.createBuffer(gl, new Float32Array([1, 0, 0, 0, 1, 1, 0, 1]));
-        GLUtil.bindAttribute(gl, this.texturePosBuffer, this.pw.texturePos, 2);
-        var textureRef = GLUtil.createTexture(gl, gl.LINEAR);
-        gl.uniform1i(this.pw.sampler, 0);
-        this.inputTextures.push(textureRef);
-      }
-    }, {
-      key: "render",
-      value: function render(data, width, height) {
-        if (data instanceof ImageData) {
-          data = data.data;
-        } else {
-          data = data[0];
-        }
-
-        var gl = this.gl;
-        var program = this.program;
-        var textureRef = this.inputTextures[0];
-        this.outputTexuture = GLUtil.createTexture(gl, gl.LINEAR, new Uint8Array(width * height * 4), width, height);
-        gl.bindFramebuffer(gl.FRAMEBUFFER, this.rend.fb);
-        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, this.outputTexuture, 0);
-        gl.useProgram(program);
-        gl.viewport(0, 0, this.canvas.width, this.canvas.height);
-        gl.activeTexture(gl.TEXTURE0);
-        gl.bindTexture(gl.TEXTURE_2D, textureRef);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, data);
-        gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-        return this.outputTexuture;
-=======
       }
 
       gl.bindFramebuffer(gl.FRAMEBUFFER, this.rend.fb);
@@ -632,7 +523,6 @@
         data = data.data;
       } else {
         data = data[0];
->>>>>>> new/live-mobile
       }
 
       let gl = this.gl;
