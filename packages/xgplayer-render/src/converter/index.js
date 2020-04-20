e46367fe0be30d2ba4e9632416acb0ba75c25dac
涂强 <tuqiang.yuri@bytedance.com>
2020-04-20 11:12:10 +0800

    feat:增加伽马校正的结果

--- packages/xgplayer-render/src/converter/index.js
+++ packages/xgplayer-render/src/converter/index.js
@@ -1,5 +1,14 @@
 import GLUtil from "../glutil";
 import Filter from "../filter";
+
+// 行主序改为gl的列主序
+const transGLOrder = (matrix) => {
+    return [
+        matrix[0],matrix[3],matrix[6],
+        matrix[1],matrix[4],matrix[7],
+        matrix[2],matrix[5],matrix[8],
+    ]
+};
 // fixme 后面抽象一下看能不能放在fmt中 作为一个可选输出
 export default class Converter extends Filter{
     constructor(config){
@@ -20,11 +29,14 @@ export default class Converter extends Filter{
             precision highp float;
             varying highp vec2 textureCoord;
             uniform sampler2D sampler;
-            uniform mat4 rgb2yuv;
+            uniform mat3 rgb2yuv;
             void main(void) {
                 vec4 color = texture2D(sampler, textureCoord);
-                vec4 yuv = vec4(color[0],color[1],color[2],color[3]) * rgb2yuv;
-                gl_FragColor = yuv;
+                vec3 yuv = vec3(color[0],color[1],color[2]) * rgb2yuv 
+                + vec3(0.5,0.5,0.5);
+                
+                
+                gl_FragColor = vec4(yuv,1.0);
                 // gl_FragColor = vec4(yuv.x,yuv.x,yuv.x,1.0);
                 // gl_FragColor = vec4(yuv.y,yuv.y,yuv.y,1.0);
             }
@@ -79,32 +91,13 @@ export default class Converter extends Filter{
             let textureRef = GLUtil.createTexture(gl, gl.LINEAR);
             gl.uniform1i(this.pw.sampler, 0);
             this.inputTextures.push(textureRef);
-            // todo 在glutil中加一个行主序的util方法
-            // let rgb2yuv = [
-            //     0.257,  0.504,  0.098,  0.0625,
-            //     0.439,  -0.368, -0.071, 0.500,
-            //     -0.148, -0.291, 0.439,  0.500,
-            //     0.0,    0.0,    0.0,    1.0
-            // ];
-            // let rgb2yuv = [
-            //     1, 1, 1, 0,
-            //     0, -0.39465, - 0.58060,0,
-            //     1, 2.032, 0,0,
-            //     0,0,0,1
-            // ];
-            // let rgb2yuv = [
-            //     0.299,-0.14731,0.615,0,
-            //     0.587, -0.28886, -0.51499,0,
-            //     0.114, 0.436, -0.10001,0,
-            //     0,0,0,1
-            // ];
+
             // 601
-            let rgb2yuv = [
-                0.299,-0.14731,0.615,0,
-                0.587, -0.28886, -0.51499,0,
-                0.114, 0.436, -0.10001,0,
-                0,0,0,1
-            ];
+            // let rgb2yuv = [
+            //     0.299,-0.14731,0.615,
+            //     0.587, -0.28886, -0.51499,
+            //     0.114, 0.436, -0.10001,
+            // ];
             // let rgb2yuv = [
             //     0.299,0.587,0.114,0,
             //     -0.14731, -0.28886,0.436,0,
@@ -125,7 +118,30 @@ export default class Converter extends Filter{
             //     0.615, -0.55861, -0.05639,0,
             //     0,0,0,1
             // ];
-            gl.uniformMatrix4fv(this.pw.rgb2yuv, false, rgb2yuv);
+            // 伽马校正
+            // let rgb2yuv = [
+            //     0.299,-0.169,0.5,
+            //     0.587, -0.331, -0.419,
+            //     0.114, 0.5, -0.081,
+            // ];
+            // let rgb2yuv = [
+            //     0.299,-0.169,0.5,
+            //     0.587, -0.331, -0.419,
+            //     0.114, 0.5, -0.081,
+            // ];
+            // let rgb2yuv = transGLOrder([
+            //     0.299,0.587,0.114,
+            //     -0.169,-0.331,0.5,
+            //     0.5,-0.419,-0.081
+            // ]);
+            // console.log(rgb2yuv2)
+            // ycbcr
+            let rgb2yuv = transGLOrder([
+                0.2568,0.504,0.1237,
+                -0.1474,-0.291,0.4392,
+                0.4392,-0.3678,-0.0714
+            ]);
+            gl.uniformMatrix3fv(this.pw.rgb2yuv, false, rgb2yuv);
 
         };
 
@@ -168,7 +184,7 @@ export default class Converter extends Filter{
                 });
             }
             return {
-                y:new Uint8ClampedArray(rs.y),
+                y:new Uint8Array(rs.y),
                 u:new Uint8Array(rs.u),
                 v:new Uint8Array(rs.v),
 
