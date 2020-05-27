660a504eff678f095547cbec2557d6a763bf78af
洪琼星 <hongqiongxing@bytedance.com>
2020-05-27 17:41:42 +0800

    fix: 修复快进时间显示时长为0问题

--- packages/xgplayer/src/plugins/mobile/index.js
+++ packages/xgplayer/src/plugins/mobile/index.js
@@ -213,7 +213,8 @@ class MobilePlugin extends Plugin {
     this.activeSeekNote(time / 1000)
     // 在滑动的同时实时seek
     if (this.config.isTouchingSeek) {
-      player.currentTime = time / 1000
+      // player.currentTime = time / 1000
+      player.seek(Number(time / 1000).toFixed(1))
     }
     this.pos.time = time
   }
@@ -235,7 +236,9 @@ class MobilePlugin extends Plugin {
   }
 
   activeSeekNote (time) {
-    if (!time || typeof time !== 'number' || this.config.disableActive) {
+    const {player, config} = this
+    const isLive = !(player.duration !== Infinity && player.duration > 0)
+    if (!time || typeof time !== 'number' || isLive || config.disableActive) {
       return
     }
     if (time < 0) {
@@ -244,9 +247,9 @@ class MobilePlugin extends Plugin {
       time = this.player.duration
     }
     this.changeAction(ACTIONS.SEEKING)
+    this.find('.dur').innerHTML = Util.format(player.duration)
     this.find('.cur').innerHTML = Util.format(time)
-    // Util.addClass(this.player.root, 'xgplayer-seeking')
-    this.find('.curbar').style.width = `${time / this.player.duration * 100}%`
+    this.find('.curbar').style.width = `${time / player.duration * 100}%`
   }
 
   switchPlayPause () {
