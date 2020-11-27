217953dfd895c5e43975763cc13be6b46aa9c7a5
fuyuhao <fuyuhao@bytedance.com>
2020-11-27 16:17:52 +0800

    chore(hls、flv): publish 3.0.0-alpha.63

--- scripts/publishTransmuxers.sh
+++ scripts/publishTransmuxers.sh
@@ -12,8 +12,8 @@ echo ${TAG_ARG}
 #lerna exec --scope xgplayer-mobilevideo -- npm publish ${TAG_ARG} --registry https://registry.npmjs.org
 #lerna exec --scope xgplayer-raw264 -- npm publish ${TAG_ARG} --registry https://registry.npmjs.org
 #lerna exec --scope xgplayer-flv-vod -- npm publish ${TAG_ARG} --registry https://registry.npmjs.org
-#lerna exec --scope xgplayer-flv-live -- npm publish ${TAG_ARG} --registry https://registry.npmjs.org
-#lerna exec --scope xgplayer-hls-vod -- npm publish ${TAG_ARG} --registry https://registry.npmjs.org
+lerna exec --scope xgplayer-flv-live -- npm publish ${TAG_ARG} --registry https://registry.npmjs.org
+lerna exec --scope xgplayer-hls-vod -- npm publish ${TAG_ARG} --registry https://registry.npmjs.org
 lerna exec --scope xgplayer-hls-live -- npm publish ${TAG_ARG} --registry https://registry.npmjs.org
 #lerna exec --scope xgplayer-hls -- npm publish ${TAG_ARG} --registry https://registry.npmjs.org
 #lerna exec --scope xgplayer-flv -- npm publish ${TAG_ARG} --registry https://registry.npmjs.org
