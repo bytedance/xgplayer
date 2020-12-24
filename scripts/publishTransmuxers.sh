f9a9f08c9c9e69dd85ffb50fb64110f532237c33
fuyuhao <fuyuhao@bytedance.com>
2020-12-24 18:23:53 +0800

    V2.3.55

--- scripts/publishTransmuxers.sh
+++ scripts/publishTransmuxers.sh
@@ -9,11 +9,11 @@ echo ${TAG_ARG}
 #lerna exec --scope xgplayer-helper-models -- npm publish ${TAG_ARG} --registry https://registry.npmjs.org
 #lerna exec --scope xgplayer-helper-transmuxers -- npm publish ${TAG_ARG} --registry https://registry.npmjs.org
 #lerna exec --scope xgplayer-helper-utils -- npm publish ${TAG_ARG} --registry https://registry.npmjs.org
-#lerna exec --scope xgplayer-mobilevideo -- npm publish ${TAG_ARG} --registry https://registry.npmjs.org
+lerna exec --scope xgplayer-mobilevideo -- npm publish ${TAG_ARG} --registry https://registry.npmjs.org
 #lerna exec --scope xgplayer-raw264 -- npm publish ${TAG_ARG} --registry https://registry.npmjs.org
 #lerna exec --scope xgplayer-flv-vod -- npm publish ${TAG_ARG} --registry https://registry.npmjs.org
-lerna exec --scope xgplayer-flv-live -- npm publish ${TAG_ARG} --registry https://registry.npmjs.org
-lerna exec --scope xgplayer-hls-vod -- npm publish ${TAG_ARG} --registry https://registry.npmjs.org
-lerna exec --scope xgplayer-hls-live -- npm publish ${TAG_ARG} --registry https://registry.npmjs.org
+#lerna exec --scope xgplayer-flv-live -- npm publish ${TAG_ARG} --registry https://registry.npmjs.org
+#lerna exec --scope xgplayer-hls-vod -- npm publish ${TAG_ARG} --registry https://registry.npmjs.org
+#lerna exec --scope xgplayer-hls-live -- npm publish ${TAG_ARG} --registry https://registry.npmjs.org
 #lerna exec --scope xgplayer-hls -- npm publish ${TAG_ARG} --registry https://registry.npmjs.org
 #lerna exec --scope xgplayer-flv -- npm publish ${TAG_ARG} --registry https://registry.npmjs.org
