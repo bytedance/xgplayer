d7088ccea400527a728ea9f82a353a3c297cc5f0
fuyuhao <fuyuhao@bytedance.com>
2021-03-02 21:03:24 +0800

    fix(live): use hls compat

--- packages/xgplayer-hls-live/src/mse/config.js
+++ packages/xgplayer-hls-live/src/mse/config.js
@@ -1,6 +1,6 @@
 import { FetchLoader, Crypto, Mse } from 'xgplayer-helper-utils'
 import { TsDemuxer, Mp4Remuxer, M3U8Parser } from 'xgplayer-helper-transmuxers';
-import { Compat as Compatibility } from 'xgplayer-helper-codec'
+import { CompatHls as Compatibility } from 'xgplayer-helper-codec'
 import { Playlist, Buffer as XgBuffer, Tracks, PreSource } from 'xgplayer-helper-models';
 
 export default {
