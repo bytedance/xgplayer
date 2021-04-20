bc459fa6b8930807dafd00870595769aa75f976f
hongqiongxing-byted <hongqiongxing@bytedance.com>
2021-04-20 11:03:22 +0800

    feat: (xgplayer) 添加关键文件的ts文件
    feat: (xgplayer) 添加关键文件的ts文件

---
+++ packages/xgplayer/scripts/tsCopy.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const recursive = require('recursive-readdir');
+const path = require('path');
+const util = require('util');
+
+const mkdirs = function (dirpath, mode, callback) {
+  if (fs.existsSync(dirpath)) {
+    callback(dirpath);
+  } else {
+    mkdirs(path.dirname(dirpath), mode, function () {
+      fs.mkdir(dirpath, mode, callback);
+    });
+  }
+};
+
+const readdir = util.promisify(recursive);
+const mkdirsPromise = util.promisify(mkdirs);
+
+const findTs = async () => {
+  const files = await readdir(path.resolve(__dirname, '../src'), []);
+  for (let i = 0; i < files.length; i++) {
+    if (files[i].indexOf('.d.ts') > -1) {
+      ['es', 'lib'].forEach((dir) => {
+        const walk = async () => {
+          const distFile = files[i].replace('src', dir);
+          const distDir = path.dirname(distFile);
+          if (!fs.existsSync(distDir)) {
+            await mkdirsPromise(distDir, {
+              recursive: true
+            });
+          }
+          const readStream = fs.createReadStream(path.resolve(files[i]));
+          const writeStream = fs.createWriteStream(distFile);
+          readStream.pipe(writeStream);
+        }
+        walk()
+      })
+    }
+  }
+}
+
+findTs()
--- packages/xgplayer/src/defaultConfig.d.ts
+++ packages/xgplayer/src/defaultConfig.d.ts
@@ -3,7 +3,7 @@
 /**
  * 插件基础配置
  */
-export interface BasePluginConfig {
+ interface BasePluginConfig {
   root?: HTMLElement; // 【可选】插件挂载的dom
   position?: String; // [可选]插件挂载的dom
   index?: Number; // [可选]插件在播放器中挂载的位置
@@ -12,7 +12,7 @@ export interface BasePluginConfig {
 /**
  * 底部控制栏配置
  */
-export interface ControlsConfig {
+interface ControlsConfig {
   root?: HTMLElement; // 【可选】插件挂载的dom
   position?: String; // [可选]插件挂载的dom
   index?: Number; // [可选]插件在播放器中挂载的位置
@@ -25,7 +25,7 @@ export interface ControlsConfig {
 /**
  * pc插件配置
  */
-export interface PCConfig {
+interface PCConfig {
   root?: HTMLElement; // 【可选】插件挂载的dom
   position?: String; // [可选]插件挂载的dom
   index?: Number; // [可选]插件在播放器中挂载的位置
@@ -35,7 +35,7 @@ export interface PCConfig {
 /**
  * mobile插件配置
  */
-export interface MobileConfig {
+interface MobileConfig {
   disableGesture?: Boolean; // 是否禁用手势, 默认false
   gestureX?: Boolean; // 是否启用水平手势,默认为true
   gestureY?: Boolean; // 是否启用垂直手势, 默认为true
@@ -62,7 +62,7 @@ export interface MobileConfig {
 /**
  * 进度条插件配置
  */
-export interface ProgressConfig {
+interface ProgressConfig {
   root?: HTMLElement; // 【可选】插件挂载的dom
   position?: String; // [可选]插件挂载的dom
   index?: Number; // [可选]插件在播放器中挂载的位置
@@ -80,7 +80,7 @@ export interface ProgressConfig {
 /**
  * 进度条预览插件配置
  */
-export interface IsPot {
+interface IsPot {
   time: Number; // 进度条在此时间戳打点 单位为s
   text?: String | Number; // 打点处的自定义文案
   id: Number; // 标记唯一标识，用于删除的时候索引
@@ -91,7 +91,7 @@ export interface IsPot {
   height?: Number;
 }
 
-export interface ProgressPreviewConfig {
+interface ProgressPreviewConfig {
   miniWidth?: Number; // 故事点显示最小宽度
   ispots: Array<IsPot>; // 故事点列表
   defaultText?: String; // 故事点hover默认文案
@@ -104,7 +104,7 @@ export interface ProgressPreviewConfig {
 /**
  * 剧情预览缩略图配置
  */
-export interface ThumbnailConfig {
+interface ThumbnailConfig {
   urls: Array<String>; // 有多张大图就多个url就好
   pic_num: Number; // 每张图含有几个雪碧图
   col: Number; // 截图列数
@@ -122,7 +122,7 @@ interface DefinitionItem {
   text: String | {};
   iconText?: String | {}; // 每个清晰度对应切换之后在切换按钮的显示文案
 }
-export interface DefinitionConfig {
+interface DefinitionConfig {
   root?: HTMLElement; // 【可选】插件挂载的dom
   position?: String; // [可选]插件挂载的dom
   index?: Number; // [可选]插件在播放器中挂载的位置
@@ -138,7 +138,7 @@ interface PlayBackRateItem {
   text: String | {};
   iconText?: String | {}; // 每个清晰度对应切换之后在切换按钮的显示文案
 }
-export interface PlayBackRateConfig {
+interface PlayBackRateConfig {
   root?: HTMLElement; // 【可选】插件挂载的dom
   position?: String; // [可选]插件挂载的dom
   index?: Number; // [可选]插件在播放器中挂载的位置
@@ -149,7 +149,7 @@ export interface PlayBackRateConfig {
 /**
  * PIP插件配置
  */
-export interface PIPConfig {
+interface PIPConfig {
   root?: HTMLElement; // 【可选】插件挂载的dom
   position?: String; // [可选]插件挂载的dom
   index?: Number; // [可选]插件在播放器中挂载的位置
@@ -159,7 +159,7 @@ export interface PIPConfig {
 /**
  * Poster插件配置
  */
-export interface PosterConfig {
+interface PosterConfig {
   isEndedShow?: Boolean; // [可选]播放结束的时候是否显示，默认true
   hideCanplay?: Boolean; // [可选]cnaplay 时间大于1的时候才隐藏,默认false
   poster: String; // 封面图地址
@@ -168,7 +168,7 @@ export interface PosterConfig {
 /**
  * enter插件配置
  */
-export interface EnterConfig {
+interface EnterConfig {
   root?: HTMLElement; // 【可选】插件挂载的dom
   position?: String; // [可选]插件挂载的dom
   index?: Number; // [可选]插件在播放器中挂载的位置
@@ -178,7 +178,7 @@ export interface EnterConfig {
 /**
  * 全屏插件配置
  */
-export interface FullscreenConfig {
+interface FullscreenConfig {
   root?: HTMLElement; // 【可选】插件挂载的dom
   position?: String; // [可选]插件挂载的dom
   index?: Number; // [可选]插件在播放器中挂载的位置
@@ -200,7 +200,7 @@ interface KeyCodeItem {
     disable?: Boolean; // 是否禁用
   }
 }
-export interface KeyboardConfig {
+interface KeyboardConfig {
   seekStep?: Number; // [可选]快捷键快进/快退每次操作时长，默认10s
   keyCodeMap?: KeyCodeItem; // [可选]是否启用旋转全屏
   disable?: Boolean; // [可选]是否禁用插件交互行为
@@ -209,7 +209,7 @@ export interface KeyboardConfig {
 /**
  * 迷你小窗配置
  */
-export interface MiniScreenConfig {
+interface MiniScreenConfig {
   disable?: Boolean; // 是否禁用
   width?: Number; // 小窗宽度，默认320
   height?: Number; // 小窗高度，默认180
@@ -224,7 +224,7 @@ export interface MiniScreenConfig {
 /**
  * 音量插件配置
  */
-export interface VolumeConfig {
+interface VolumeConfig {
   root?: HTMLElement; // 【可选】插件挂载的dom
   position?: String; // [可选]插件挂载的dom
   index?: Number; // [可选]插件在播放器中挂载的位置
@@ -236,7 +236,7 @@ export interface VolumeConfig {
  * 外挂字幕插件配置
  */
 
-export interface Texttrack {
+interface Texttrack {
   src: String; // 外挂字幕地址
   text?: String | Object; // 外挂字幕地址
   language?: String; // 外挂字幕地址
@@ -244,7 +244,7 @@ export interface Texttrack {
   id?: Number | String
 }
 
-export interface TexttrackStyle {
+interface TexttrackStyle {
   follow?: Boolean; // 是否跟随控制栏调整位置;默认true
   mode?: String; // 字体显示模式，默认stroke(描边)
   followBottom?: Number; // 跟随底部控制栏的高度
@@ -255,10 +255,10 @@ export interface TexttrackStyle {
   minSize?: Number; // pc端最小字号; 默认16
   minMobileSize?: Number; // 移动端最小字号; 默认13
   line?: String; // 最大显示行数 single/double/three; 默认double
-  fontColor?: String // 字体颜色; 默认#fff
+  fontColor?: String // 字体颜色; 默认#fff,
 }
 
-export interface TextTrackConfig {
+interface TextTrackConfig {
   root?: HTMLElement; // 【可选】插件挂载的dom
   position?: String; // [可选]插件挂载的dom
   index?: Number; // [可选]插件在播放器中挂载的位置
@@ -271,14 +271,14 @@ export interface TextTrackConfig {
   };
 }
 
-export interface StartConfig {
+interface StartConfig {
   isShowPause?: Boolean; // 暂停是否常驻, 默认false
   isShowEnd?: Boolean; // 播放结束常驻, 默认false
   disableAnimate?: Boolean; // 禁用点击动画， 默认false
   mode?: 'hide' | 'show' | 'auto'// 控制模式: hide 常驻: show 跟随：auto, 默认hide
 }
 
-export interface RotateConfig {
+interface RotateConfig {
   root?: HTMLElement; // 【可选】插件挂载的dom
   position?: String; // [可选]插件挂载的dom
   index?: Number; // [可选]插件在播放器中挂载的位置
@@ -287,7 +287,7 @@ export interface RotateConfig {
   rotateDeg?: Number; // 初始旋转角度, 默认0
 }
 
-export interface ScreenShotConfig {
+interface ScreenShotConfig {
   root?: HTMLElement; // 【可选】插件挂载的dom
   position?: String; // [可选]插件挂载的dom
   index?: Number; // [可选]插件在播放器中挂载的位置
@@ -299,3 +299,294 @@ export interface ScreenShotConfig {
   fitVideo?: Boolean; // [可选]截取图片是否适配视频宽高,默认true,
   name?: String; // [可选]截图存储命名,默认'截图'
 }
+
+type DanmuModelType = 'top' | 'bottom' | 'scroll' | string;
+
+interface DanmuCommentOptions {
+  duration: number;
+  id: any;
+  start: number;
+  prior?: boolean;
+  color?: boolean;
+  txt: string;
+  // eslint-disable-next-line no-undef
+  style?: Record<string, string | number>;
+  mode?: DanmuModelType;
+}
+
+interface DanmuOptions {
+  comments: DanmuCommentOptions[];
+  panel?: boolean;
+  area?: {
+      start: number;
+      end: number;
+  };
+  closeDefaultBtn?: boolean;
+  defaultOff?: boolean;
+}
+
+interface IPlayerConfigs {
+
+  // 选择器
+  id?: string;
+
+  // DOM实例
+  // eslint-disable-next-line no-undef
+  el?: HTMLElement;
+
+  // 视频源
+  url: string | Array<{src: string; type?: string}>;
+
+  // 宽度(默认600)
+  width?: number | string;
+
+  // 高度（默认337.5）
+  height?: number | string;
+
+  // 流式布局（默认false）
+  // 设置为流式布局，可使播放器宽度跟随父元素的宽度大小变化，且高度按照配置项中的高度和宽度的比例来变化
+  fluid?: boolean;
+
+  // 自适应视频内容宽高,可设置三种模式：
+  //  1、fitVideoSize设置为fixWidth，表示容器宽度固定，容器高度按照视频内容比例增大或减小；
+  //  2、fitVideoSize设置为fixHeight，表示容器高度固定，容器宽度按照视频内容比例增大或减小；
+  //  3、fitVideoSize设置为auto，表示容器高度或宽度按照视频内容比例增大
+  fitVideoSize?: 'fixWidth' | 'fixHeight' | 'auto';
+  /**
+   * video画面填充模式
+   */
+  videoFillMode?: 'fillHeight' | 'fillWidth' | 'fill' | 'auto';
+
+  /**
+   * 音量配置
+   * 可以是音量值/音量插件的配置对象/是否显示音量插件
+   */
+  volume?: number | VolumeConfig;
+
+  // 自动播放（默认false）
+  autoplay?: boolean;
+  // 默认静音自动播放
+
+  autoplayMuted?: boolean;
+
+  // 循环播放(默认false)
+  loop?: boolean;
+
+  // 初始化显示视频首帧（默认false,该配置在移动端无效)
+  videoInit?: boolean;
+
+  // 封面图 或者是封面图的配置对象
+  poster?: string | PosterConfig;
+
+  // 是否启用移动端模拟状态，该状态为true则展示的是移动端效果
+  isMobileSimulateMode?: boolean;
+
+  // 倍速播放
+  // 播放器支持设置视频的当前播放速度。可通过defaultPlaybackRate设置初始速度。
+  playbackRate?: Boolean | Array<Number> | PlayBackRateConfig;
+
+  // 默认播放速度
+  defaultPlaybackRate?: number;
+
+  // 预览本地视频
+  // eslint-disable-next-line no-undef
+  preview?: {uploadEl?: HTMLElement};
+
+  // 进度条特殊点标记
+  progressDot?: Array<IsPot>;
+
+  // 键盘快捷键 默认值：'on'
+  keyShortcut?: boolean;
+
+  // 插件生效前执行
+  execBeforePluginsCall?: () => void;
+
+  // 关闭video触发click事件后视频切换播放/暂停状态
+  closeVideoClick?: boolean;
+
+  // 关闭video触发dblclick事件后进入/退出全屏
+  closeVideoDblclick?: boolean;
+
+  // 关闭移动端下默认video触发touchend事件后视频切换播放/暂停状态
+  closeVideoTouch?: boolean;
+
+  // 关闭鼠标移出播放器范围时触发blur事件
+  closePlayerBlur?: boolean;
+
+  // 延迟触发blur事件的时间
+  leavePlayerTime?: number;
+
+  // 是否关闭自动隐藏控制条
+  closeDelayBlur?: boolean;
+
+  // 关闭鼠标移出播放器控制条范围时触发focus事件
+  closeControlsBlur?: boolean;
+
+  // 关闭播放器范围时移动鼠标时触发video focus
+  closeFocusVideoFocus?: boolean;
+
+  // 是否关闭pause时触发focus
+  closePauseVideoFocus?: boolean;
+
+  // 关闭播放器触发play事件时触发video focus
+  closePlayVideoFocus?: boolean;
+
+  // 清晰度切换配置
+  definitionActive?: 'hover' | 'click';
+
+  // 关闭内置控件
+  // eslint-disable-next-line max-len
+  ignores?: Array<'cssfullscreen' | 'screenshot' | 'pip' | 'miniscreen' | 'keyboard' | 'download' | 'playbackrate' | 'time' | 'definition' | 'error' | 'fullscreen' | 'loading' | 'mobile' | 'pc' | 'play' | 'poster' | 'progress' | 'replay' | 'start' | 'volume' | string>;
+
+  // 播放镜像 https://support.apple.com/en-us/HT204289
+  airplay?: boolean;
+
+  // 国际化
+  lang?: 'zh-cn' | 'en' | 'jp' | 'zh';
+
+  // 内联模式 https://webkit.org/blog/6784/new-video-policies-for-ios/
+  // 该选项在手机观看时，开启ios和微信的内联模式
+  playsinline?: boolean;
+
+  // 手机调试
+  // 要使用该功能，请先在开发者电脑安装 weinre
+  debug?: {host: string; port: number};
+
+  // 微信同层播放
+  'x5-video-player-type'?: 'h5';
+
+  // 微信全屏播放
+  'x5-video-player-fullscreen'?: boolean;
+
+  // 微信横竖屏控制
+  'x5-video-orientation'?: 'landscape' | 'portraint';
+
+  // 自定义配置
+  customConfig?: {
+    [propName: string]: any;
+  };
+
+  // 关闭控制条， 默认true
+  controls?: Boolean | ControlsConfig;
+
+  // 控制栏是否位于画面底部，不与画面重合
+  marginControls?: Boolean;
+
+  // 需要安装的插件列表
+  plugins?: any[];
+
+  // 需要使用的preset列表
+  presets?: any[];
+
+  // video标签扩展属性
+  videoAttrbutes?: any;
+
+  // 按钮配置列表
+  icons?: any;
+
+  // 用于配置一些通用样式结构
+  commonStyle?: {
+      // 进度条底色
+      progressColor?: string,
+      // 播放完成部分进度条底色
+      playedColor?: string,
+      // 播放完成部分进度条底色
+      cachedColor?: string,
+      // 进度条滑块样式
+      sliderBtnStyle?: any,
+      // 进度条滑块样式
+      volumeColor?: string
+    };
+
+  // 进度条自动消失延时
+  inactive?: number;
+
+  // 自动播放起始时间点
+  startTime?: number;
+
+  // 【即将废弃，后续使用seekedStatus】seek结束之后是否默认播放
+  isSeekedPlay?: boolean;
+
+  // 快进/快退之后播放器状态 play: 强制播放; pause: 强制暂停; auto: 保持状态
+  seekedStatus?: 'play' | 'pause' | 'auto';
+
+  // 是否添加迷你控制栏
+  miniprogress?: boolean;
+
+  // 移动端滑动进行快进/快退开始时回调
+  disableSwipeHandler?: () => void;
+
+  enableSwipeHandler?: () => void;
+
+  // 清晰度切换插件配置
+  definition?: Boolean | DefinitionConfig | any;
+
+  // 预览
+  thumbnail?: ThumbnailConfig;
+
+  // 下一集
+  playNext?: {
+      urlList: string[];
+  };
+
+  // 视频旋转按钮配置项
+  rotate?: boolean | RotateConfig;
+
+  // 视频下载(默认false)
+  download?: boolean | BasePluginConfig;
+
+  // 弹幕（具体用法参考https://github.com/bytedance/danmu.js）
+  danmu?: DanmuOptions;
+
+  // 外挂字幕（参考https://developer.mozilla.org/en-US/docs/Web/API/WebVTT_API）
+  textTrack?: boolean | TextTrackConfig;
+
+  // 外挂字幕样式
+  textTrackStyle?: TexttrackStyle;
+
+  // 配置项修改外挂字幕控件的触发方式
+  textTrackActive?: 'hover' | 'click';
+
+  // 画中画
+  pip?: boolean | PIPConfig;
+
+  // 网页样式全屏
+  cssFullscreen?: boolean | any;
+
+  // 全屏插件配置
+  fullscreen?: boolean | FullscreenConfig;
+
+  // 截图
+  screenShot?: boolean | ScreenShotConfig;
+
+  // 移动端手势插件配置
+  mobile?: MobileConfig;
+
+  // 迷你小屏播放插件配置
+  mini?: boolean | MiniScreenConfig;
+
+  // pc端快捷键配置
+  keyboard?: Boolean | KeyboardConfig;
+
+  enter?: boolean | EnterConfig;
+
+  // 进度条预览插件
+  progressPreview?: ProgressPreviewConfig;
+
+  // 进度条插件配置
+  progress?: boolean | ProgressConfig;
+
+  // 中间播放按钮插件配置
+  start?: boolean | StartConfig;
+
+  // pc相关插件配置
+  pc?: PCConfig;
+
+  // video属性，该配置会直接添加到video上
+  videoConfig?: any;
+
+  // 扩展定义
+  [propName: string]: any;
+}
+
+export default IPlayerConfigs
