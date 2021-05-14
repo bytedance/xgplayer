d3560d27e0b4c8c382b60af7749002b57cd5b72f
hongqiongxing-byted <hongqiongxing@bytedance.com>
2021-05-14 17:40:31 +0800

    feat:(xgplayer) touch增加preventDefault,解决ios端双击无法触发问题

--- packages/xgplayer/src/plugins/mobile/touch.js
+++ packages/xgplayer/src/plugins/mobile/touch.js
@@ -59,7 +59,6 @@ class Touche {
     this.dbIntrvalId = null
     this.__handlers = {}
     this._initEvent()
-    this.timstamp = parseInt(window.performance.now(), 10)
   }
 
   _initEvent () {
@@ -67,7 +66,6 @@ class Touche {
     this.onTouchMove = this.onTouchMove.bind(this)
     this.onTouchEnd = this.onTouchEnd.bind(this)
     this.onTouchCancel = this.onTouchCancel.bind(this)
-    // this.root.addEventListener('touchstart', this.onTouchStart)
     this.root.addEventListener(this.events.start, this.onTouchStart)
   }
 
@@ -98,13 +96,11 @@ class Touche {
   __setDb (e) {
     const {config} = this
     if (this.dbIntrvalId) {
-      // console.log('__clearDb0')
       this.__clearDb()
       this.trigger(EVENTS.DOUBlE_CLICK, e)
       return
     }
     this.dbIntrvalId = setTimeout(() => {
-      // console.log('__clearDb1')
       this.__clearDb()
       if (!this._pos.start && !this._pos.press && !this._pos.moving) {
         this.trigger(EVENTS.CLICK, e)
@@ -113,7 +109,6 @@ class Touche {
   }
 
   __clearDb () {
-    // console.log('__clearDb', this.dbIntrvalId)
     clearTimeout(this.dbIntrvalId)
     this.dbIntrvalId = null
   }
@@ -160,18 +155,13 @@ class Touche {
   }
 
   onTouchStart (e) {
-    const time = parseInt(window.performance.now(), 10)
-    console.log('onTouchStart', time - this.timstamp)
-    this.timstamp = time
     const {_pos, root} = this
+    e.preventDefault();
     const touch = getTouch(e.touches)
     _pos.x = touch ? parseInt(touch.pageX, 10) : e.pageX
     _pos.y = touch ? parseInt(touch.pageX, 10) : e.pageX
     _pos.start = true
     this.__setPress(e)
-    // root.addEventListener('touchend', this.onTouchEnd)
-    // root.addEventListener('touchcancel', this.onTouchCancel)
-    // root.addEventListener('touchmove', this.onTouchMove)
     root.addEventListener(this.events.end, this.onTouchEnd)
     root.addEventListener(this.events.cancel, this.onTouchCancel)
     root.addEventListener(this.events.move, this.onTouchMove)
@@ -179,17 +169,13 @@ class Touche {
   }
 
   onTouchCancel (e) {
-    const time = parseInt(window.performance.now(), 10)
-    console.log('onTouchCancel', time - this.timstamp)
-    this.timstamp = time
     this.onTouchEnd(e)
   }
 
   onTouchEnd (e) {
     const {_pos, root} = this
+    e.preventDefault();
     this.__clearPress()
-    // root.removeEventListener('touchend', this.onTouchEnd)
-    // root.removeEventListener('touchmove', this.onTouchMove)
     root.removeEventListener(this.events.cancel, this.onTouchCancel)
     root.removeEventListener(this.events.end, this.onTouchEnd)
     root.removeEventListener(this.events.move, this.onTouchMove)
@@ -197,25 +183,19 @@ class Touche {
     e.press = _pos.press
     _pos.press && this.trigger(EVENTS.PRESS_END, e)
     this.trigger(EVENTS.TOUCH_END, e)
-    // console.log('onTouchEnd', `press:${_pos.press} moving:${_pos.moving}`)
-    const time = parseInt(window.performance.now(), 10)
-    console.log('onTouchEnd', time - this.timstamp)
-    this.timstamp = time
-    !_pos.press && !_pos.moving && this.__setDb() && console.log('__setDb')
+    !_pos.press && !_pos.moving && this.__setDb()
     _pos.press = false
     _pos.start = false
     _pos.moving = false
   }
 
   onTouchMove (e) {
-    console.log('onTouchMove')
     const {_pos, config} = this
     const touch = getTouch(e.touches)
     const x = touch ? parseInt(touch.pageX, 10) : e.pageX
     const y = touch ? parseInt(touch.pageY, 10) : e.pageX
     const diffx = x - _pos.x
     const diffy = y - _pos.y
-    // console.log(`diffx: ${diffx} diffy:${diffy}`)
     if (Math.abs(diffy) < config.miniStep && Math.abs(diffx) < config.miniStep) {
       return
     }
