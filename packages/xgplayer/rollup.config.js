3fef84f248e929229d91f764248844c4b927cdaf
hongqiongxing-byted <hongqiongxing@bytedance.com>
2021-06-04 18:13:43 +0800

    chore:(xgplayer) 更新rollup配置

--- packages/xgplayer/rollup.config.js
+++ packages/xgplayer/rollup.config.js
@@ -1,18 +1,18 @@
 const commonRollup = require('../../rollup.config');
-const uglify = process.env.NODE_ENV === 'production';
+const uglify =  process.env.NODE_ENV === 'production';
 const svg = require('rollup-plugin-svg-import');
 
-module.exports = commonRollup({
+const umd = commonRollup({
   name: 'Player',
   uglify: uglify,
   input: 'src/index-umd.js',
   targets: {
     'chrome': '58',
-    'ie': '11'
+    // 'ie': '11'
   },
-  // babel: {
-  //   babelrc: false,
-  // },
+  babel: {
+    babelrc: false,
+  },
   plugins: [
     svg({
       // process SVG to DOM Node or String. Default: false
@@ -20,3 +20,4 @@ module.exports = commonRollup({
     })
   ]
 })
+module.exports = [umd]
