20fcea552444f1c0d4048cf2ecc4579b1e72176c
hongqiongxing-byted <hongqiongxing@bytedance.com>
2021-06-23 15:01:59 +0800

    chore(xgplayer) script/ eslint

--- packages/xgplayer/scripts/buildBrowser.js
+++ packages/xgplayer/scripts/buildBrowser.js
@@ -1,29 +1,29 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
 async function mkdirs (dirpath, mode, callback) {
   if (fs.existsSync(dirpath)) {
-    callback(dirpath);
+    callback(dirpath)
   } else {
     mkdirs(path.dirname(dirpath), mode, function () {
-      fs.mkdir(dirpath, mode, callback);
-    });
+      fs.mkdir(dirpath, mode, callback)
+    })
   }
 };
 
 const copy = async (entry) => {
-  const distDir = path.resolve(__dirname, `../browser/`);
-  const srcDir = path.resolve(__dirname, `../dist/`);
+  const distDir = path.resolve(__dirname, '../browser/')
+  const srcDir = path.resolve(__dirname, '../dist/')
   if (!fs.existsSync(distDir)) {
     await mkdirs(distDir, {
       recursive: true
-    }, () => {});
+    }, () => {})
   }
-  await fs.writeFileSync(`${distDir}/${entry}`, fs.readFileSync(`${srcDir}/${entry}`));
-};
+  await fs.writeFileSync(`${distDir}/${entry}`, fs.readFileSync(`${srcDir}/${entry}`))
+}
 
-const files = ['index.min.js', 'xgplayer.min.css'];
+const files = ['index.min.js', 'xgplayer.min.css']
 
 files.map(async item => {
-  await copy(item);
-});
+  await copy(item)
+})
