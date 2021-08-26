a24611ff10a26e972098b8123e3223a01f8fab3e
hongqiongxing-byted <hongqiongxing@bytedance.com>
2021-08-26 16:01:08 +0800

    style(xgplayer): xgplayer@3.0.0-rc.1

--- packages/xgplayer/types/index.d.ts
+++ packages/xgplayer/types/index.d.ts
@@ -486,7 +486,6 @@ declare module "events" {
 }
 declare module "proxy" {
     export default VideoProxy;
-    export type EventEmitter = any;
     export type IVideoProxy = {
         duration: number;
         currentTime: number;
@@ -505,9 +504,6 @@ declare module "proxy" {
         play: Function;
         pause: Function;
     };
-    /**
-     * @extends { EventEmitter }
-     */
     class VideoProxy {
         /**
          * @param {any} options
@@ -732,6 +728,36 @@ declare module "proxy" {
          * @description 设置/返回视频的音量
          */
         get volume(): number;
+        /** ******************* 以下api只有申明作用,具体实现依赖EventEmitter ******************/
+        /**
+         *
+         * @param { string } event
+         * @param { any } [data]
+         * @returns
+         */
+        emit(event: string, data?: any, ...args: any[]): void;
+        /**
+         *
+         * @param { string } event
+         * @param { (data?: any) => any } callback
+         * @returns
+         */
+        on(event: string, callback: (data?: any) => any, ...args: any[]): void;
+        /**
+         *
+         * @param { string } event
+         * @param { (data?: any) => any } callback
+         * @returns
+         */
+        once(event: string, callback: (data?: any) => any, ...args: any[]): void;
+        /**
+         *
+         * @param { string } event
+         * @param { (data?: any) => any } callback
+         * @returns
+         */
+        off(event: string, callback: (data?: any) => any, ...args: any[]): void;
+        removeListener(): void;
     }
 }
 declare module "utils/database" {
