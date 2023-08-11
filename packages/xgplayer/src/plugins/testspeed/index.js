b4620c5f1515f87f0d24c2a32a54e3187312d55f
李璐 <lilu.only@bytedance.com>
2023-08-11 13:25:56 +0800

    testSpeed support other type test

--- packages/xgplayer/src/plugins/testspeed/index.js
+++ packages/xgplayer/src/plugins/testspeed/index.js
@@ -1,5 +1,6 @@
 import BasePlugin, { Events, Util } from '../../plugin'
-
+const DEFAULT_SPEED_TYPE = 'cdn'
+const SPEED_TYPE = ['cdn'] // 默认的速度的种类，可以通过addSpeedTypeList再继续增加
 export default class TestSpeed extends BasePlugin {
   static get pluginName () {
     return 'testspeed'
@@ -12,42 +13,53 @@ export default class TestSpeed extends BasePlugin {
       loadSize: 200 * 1024, // 一次播放的测速请求的数据量大小，单位字节，默认200k
       testTimeStep: 3000, // 每次测速的时间间隔，第一次在开播后的testTimeStep 秒，默认是3s
       url: '', // 每次测速的url，一般传播放的备用地址测速
-      saveSpeedMax: 5 // 保留最近多少次测速记录，最后的速度求这个记录列表平均值,默认5条
+      saveSpeedMax: 5, // 保留最近多少次测速记录，最后的速度求这个记录列表平均值,默认5条
+      addSpeedTypeList:[] // 需要增加的speed的类型
     }
   }
 
-  getSpeed = (speedList) => {
-    if (this.speedListCache.length <= 0) return 0
+  getSpeed = (type = DEFAULT_SPEED_TYPE) => {
+    if (this.speedListCache[type].length <= 0) return 0
     let total = 0
-    this.speedListCache.map(item => {
+    this.speedListCache[type].map(item => {
       total += item
     })
-    return Math.floor(total / this.speedListCache.length)
+    return Math.floor(total / this.speedListCache[type].length)
   }
 
   afterCreate () {
-    this.speedListCache = []
-    const { openSpeed, testTimeStep } = this.config
+    const { openSpeed, addSpeedTypeList } = this.config
+    if (addSpeedTypeList?.length > 0) {
+      SPEED_TYPE.push(...addSpeedTypeList)
+    }
+    this.initSpeedList()
+    this.on('real_time_speed', this._onRealSpeedChange)
     this.timer = null
     this.cnt = 0
     this.xhr = null
     if (!openSpeed) {
       return
     }
-    this.on('real_time_speed', this._onRealSpeedChange)
+    this.on([Events.LOADED_DATA, Events.REPLAY], this.startTimer)
+  }
+  startTimer = () => {
+    if (Util.isMSE(this.player.video)) {
+      return
+    }
+    this.initSpeedList()
+    this.cnt = 0
+    this.timer = setTimeout(this.testSpeed, this.config.testTimeStep)
+  }
 
-    this.on([Events.LOADED_DATA, Events.REPLAY], () => {
-      if (Util.isMSE(this.player.video)) {
-        return
-      }
-      this.speedListCache = []
-      this.cnt = 0
-      this.timer = setTimeout(this.testSpeed, testTimeStep)
+  initSpeedList = () => {
+    this.speedListCache = {}
+    SPEED_TYPE.forEach((type)=> {
+      this.speedListCache[type] = []
     })
   }
 
   _onRealSpeedChange = (data) => {
-    data.speed && this.appendList(data.speed)
+    data.speed && this.appendList(data.speed, data.type || DEFAULT_SPEED_TYPE)
   }
 
   testSpeed = () => {
@@ -90,22 +102,39 @@ export default class TestSpeed extends BasePlugin {
     }
   }
 
-  appendList = (speed) => {
+  appendList = (speed, type = DEFAULT_SPEED_TYPE) => {
     const { saveSpeedMax } = this.config
-    if (this.speedListCache.length >= saveSpeedMax) {
-      this.speedListCache.shift()
+    if (this.speedListCache[type].length >= saveSpeedMax) {
+      this.speedListCache[type].shift()
     }
-    this.speedListCache.push(speed)
-    this.player && (this.player.realTimeSpeed = speed)
-    this.updateSpeed()
+    this.speedListCache[type].push(speed)
+    const { player } = this
+    if (player) {
+      type === DEFAULT_SPEED_TYPE ? (player.realTimeSpeed = speed) : (player[this.getSpeedName('realTime', type)] = speed)
+    }
+    this.updateSpeed(type)
   }
 
-  updateSpeed = () => {
-    const speed = this.getSpeed()
+  getSpeedName (namePrefix, type){
+    return namePrefix + 'Speed' + type.toUpperCase()
+  }
+
+  updateSpeed = (type = DEFAULT_SPEED_TYPE) => {
+    const speed = this.getSpeed(type)
     const { player } = this
-    if (player && (!player.avgSpeed || speed !== player.avgSpeed)) {
-      player.avgSpeed = speed
-      player.emit(Events.DOWNLOAD_SPEED_CHANGE, { speed: speed, realTimeSpeed: player.realTimeSpeed })
+    if (player) {
+      if (type === DEFAULT_SPEED_TYPE) {
+        if ((!player.avgSpeed || speed !== player.avgSpeed)) {
+          player.avgSpeed = speed
+          player.emit(Events.DOWNLOAD_SPEED_CHANGE, {speed: speed, realTimeSpeed: player.realTimeSpeed})
+        }
+      } else {
+        const speedName = this.getSpeedName('avg', type)
+        if (!player[speedName] || speed !== player[speedName]) {
+          player[speedName] = speed
+          player.emit(Events.DOWNLOAD_SPEED_CHANGE, {speed: speed, realTimeSpeed: player.realTimeSpeed})
+        }
+      }
     }
   }
 
@@ -127,10 +156,16 @@ export default class TestSpeed extends BasePlugin {
   }
 
   destroy () {
+    this.off('real_time_speed', this._onRealSpeedChange)
+    this.off([Events.LOADED_DATA, Events.REPLAY], this.startTimer)
+    SPEED_TYPE.forEach((type)=> {
+      this.speedListCache[type] = []
+    })
+    this.speedListCache = {}
     clearTimeout(this.timer)
     this.timer = null
     if (this.xhr && this.xhr.readyState !== 4) {
-      this.xhr.cancel()
+      this.xhr.cancel && this.xhr.cancel()
       this.xhr = null
     }
   }
