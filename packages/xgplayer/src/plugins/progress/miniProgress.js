6e9a8c647b8476f56d79de17f96d8a5683d2e3a2
hongqiongxing <hongqiongxing@126.com>
2023-11-20 15:19:30 +0800

    fix(xgplayer): miniprogress增加颜色配置响应 close https://github.com/bytedance/xgplayer/issues/1179

--- packages/xgplayer/src/plugins/progress/miniProgress.js
+++ packages/xgplayer/src/plugins/progress/miniProgress.js
@@ -1,4 +1,9 @@
 import Plugin from '../../plugin'
+
+function getBgColor (color) {
+  return color ? `background:${color};` : ''
+}
+
 class MiniProgress extends Plugin {
   static get pluginName () {
     return 'MiniProgress'
@@ -27,9 +32,15 @@ class MiniProgress extends Plugin {
     if (!this.playerConfig.progress || !this.playerConfig.miniprogress) {
       return
     }
-    return `<xg-mini-progress class="xg-mini-progress">
-    <xg-mini-progress-cache class="xg-mini-progress-cache"></xg-mini-progress-cache>
-    <xg-mini-progress-played class="xg-mini-progress-played"></xg-mini-progress-played>
+    const { commonStyle } = this.playerConfig
+    const _style = {
+      cached: getBgColor(commonStyle.cachedColor),
+      played: getBgColor(commonStyle.playedColor),
+      progress: getBgColor(commonStyle.progressColor),
+    }
+    return `<xg-mini-progress class="xg-mini-progress" style="${_style.progress}">
+    <xg-mini-progress-cache class="xg-mini-progress-cache" style="${_style.cached}"></xg-mini-progress-cache>
+    <xg-mini-progress-played class="xg-mini-progress-played" style="${_style.played}"></xg-mini-progress-played>
     </xg-mini-progress>`
   }
 }
