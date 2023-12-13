0eb9b75ea90f71e77c14c9591ae6ed90e521fefa
lixiangfei <lixiangfei@bytedance.com>
2023-12-13 10:48:02 +0800

    fix(xgplayer): 1. fix progress dot position 2. add mediaSrc attribute when report error

--- packages/xgplayer/src/plugins/progress/index.js
+++ packages/xgplayer/src/plugins/progress/index.js
@@ -710,6 +710,7 @@ class Progress extends Plugin {
     this.innerList.update({ played: 0, cached: 0 }, 0)
     const { miniprogress } = this.player.plugins
     miniprogress && miniprogress.update({ cached: 0, played: 0 }, 0)
+    this.progressBtn.style.left = '0%'
   }
 
   destroy () {
