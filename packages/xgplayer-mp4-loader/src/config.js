2dda8f70c5b8c1ee545dd5db1cb81058d73a1c2c
gemstone <gems.xu@gmail.com>
2023-12-19 19:34:49 +0800

    fix(mp4): 修复 MP4 音画不同步问题

--- packages/xgplayer-mp4-loader/src/config.js
+++ packages/xgplayer-mp4-loader/src/config.js
@@ -5,6 +5,7 @@ export function getConfig (cfg) {
     segmentDuration: 2,
     maxDownloadInfoSize: 30,
     responseType: 'arraybuffer',
+    fixEditListOffset: true,
     cache: null,
     // ...xgplayer-streaming-shared/src/net/config
     ...cfg
