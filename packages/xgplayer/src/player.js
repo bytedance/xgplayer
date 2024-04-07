1e61a9d9e57a601e7622c94183d7d704f413fdf3
hongqiongxing <hongqiongxing@126.com>
2024-04-07 20:21:17 +0800

    fix(xgplayer): 修复root为空的时候destroy调用异常问题

--- packages/xgplayer/src/player.js
+++ packages/xgplayer/src/player.js
@@ -1258,12 +1258,12 @@ class Player extends MediaProxy {
    */
   destroy () {
     const { innerContainer, root, media } = this
-    if (!root || !media) {
+    if (!media) {
       return
     }
     this.hasStart = false
     this._useAutoplay = false
-    root.removeAttribute(PLATER_ID)
+    root && root.removeAttribute(PLATER_ID)
     this.updateAcc('destroy')
     this._unbindEvents()
     this._detachSourceEvents(this.media)
@@ -1273,7 +1273,7 @@ class Player extends MediaProxy {
     delHooksDescriptor(this)
     super.destroy()
     // 退出全屏
-    if (this.fullscreen && this._fullscreenEl === this.root) {
+    if (this.fullscreen && this._fullscreenEl === root) {
       this.exitFullscreen()
     }
 
@@ -1283,7 +1283,7 @@ class Player extends MediaProxy {
         innerContainer.removeChild(_c[i])
       }
     }
-    !innerContainer &&
+    root && !innerContainer &&
     media instanceof window.Node &&
       root.contains(media) &&
       root.removeChild(media);
@@ -1291,14 +1291,14 @@ class Player extends MediaProxy {
       this[item] && root.removeChild(this[item])
       this[item] = null
     })
-    const cList = root.className.split(' ')
-    const isBuildIn = Util.hasClass(this.root, STATE_CLASS.BUILTIN_CLASS)
+    const cList = root ? root.className.split(' ') : []
+    const isBuildIn = root && Util.hasClass(root, STATE_CLASS.BUILTIN_CLASS)
     if (cList.length > 0) {
       root.className = cList
         .filter((name) => name.indexOf('xgplayer') < 0)
         .join(' ')
     } else {
-      root.className = ''
+      root && (root.className = '')
     }
     this.removeAttribute('data-xgfill');
 
