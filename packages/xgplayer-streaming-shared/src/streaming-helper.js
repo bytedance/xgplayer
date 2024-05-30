c7dc56b3285fafa9c9ea15862bf79b5ad754b3b2
李璐 <lilu.only@bytedance.com>
2024-05-30 14:17:49 +0800

    fix(mp4-loader): report Uint8Array allocation failures

--- packages/xgplayer-streaming-shared/src/streaming-helper.js
+++ packages/xgplayer-streaming-shared/src/streaming-helper.js
@@ -1,3 +1,4 @@
+export const NEW_ARRAY_MAX_CNT = 10
 export function isMediaPlaying (media) {
   return media && !media.paused && !media.ended && media.playbackRate !== 0 && media.readyState !== 0
 }
@@ -27,7 +28,25 @@ export function getVideoPlaybackQuality (video) {
 export function concatUint8Array (...arr) {
   arr = arr.filter(Boolean)
   if (arr.length < 2) return arr[0]
-  const data = new Uint8Array(arr.reduce((p, c) => p + c.byteLength, 0))
+  const size = arr.reduce((p, c) => p + c.byteLength, 0)
+  let tryCnt = 0
+  let data
+  while (tryCnt < NEW_ARRAY_MAX_CNT) {
+    try {
+      data = new Uint8Array(size)
+      if (data && data.byteLength > 0) {
+        break
+      } else {
+        tryCnt++
+      }
+    } catch (e) {
+      if (tryCnt < NEW_ARRAY_MAX_CNT) {
+        tryCnt++
+      } else {
+        throw new Error(`new array failed final,${e?.message}`)
+      }
+    }
+  }
   let prevLen = 0
   arr.forEach((d) => {
     data.set(d, prevLen)
