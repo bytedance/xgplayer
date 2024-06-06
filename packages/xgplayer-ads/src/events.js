4b220637373e63a29125e64c97261fe285a8d565
gemstone <gems.xu@gmail.com>
2024-06-06 21:29:56 +0800

    refactor: 💡 (xgplayer-ads) 完善IMA SDK集成能力

---
+++ packages/xgplayer-ads/src/events.js
@@ -0,0 +1,15 @@
+
+export const AD_PLAY = 'adPlay'
+export const AD_PAUSE = 'adPause'
+
+// IMA Specific Events
+export const IMA_AD_MANAGER_LOADED = 'ima_ad_manager_loaded'
+export const IMA_AD_PAUSE = 'ima_ad_pause'
+export const IMA_AD_ENDED = 'ima_ad_ended'
+export const IMA_AD_SKIPPED = 'ima_ad_skipped'
+export const IMA_AD_COMPLETE = 'ima_ad_complete'
+export const IMA_AD_ERROR = 'ima_ad_error'
+export const IMA_AD_TIME_UPDATE = 'ima_ad_time_update'
+export const IMA_AD_VOLUME_CHANGE = 'ima_ad_volume_change'
+export const IMA_AD_SEEKING = 'ima_ad_seeking'
+export const IMA_AD_SEEKED = 'ima_ad_seeked'
