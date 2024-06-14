b97c937b16bae227d2cf7d8137cc12dd8f29a8cc
gemstone <gems.xu@gmail.com>
2024-06-14 19:56:36 +0800

    docs: ✏️ (xgplayer-ads) 更新readme

--- fixtures/ads/index.js
+++ fixtures/ads/index.js
@@ -1,4 +1,4 @@
-import AdPlugin from '../../packages/xgplayer-ads/src'
+import AdPlugin, { AdEvents } from '../../packages/xgplayer-ads/src'
 import { Logger } from '../../packages/xgplayer-streaming-shared/src/logger'
 import Player from '../../packages/xgplayer/src/index.umd'
 
@@ -11,36 +11,37 @@ window.player = new Player({
   autoplay: true,
   width: '80%',
   height: 700,
-  ignores:[],
+  ignores: [],
   plugins: [AdPlugin],
   ad: {
     adType: 'ima'
   }
 })
 
-player.on('adPlay', function (e) {
+player.on(AdEvents.AD_PLAY, function (e) {
   console.log('=====> AD_PLAY', e)
 })
 
-player.on('adPause', function (e) {
+player.on(AdEvents.AD_PAUSE, function (e) {
   console.log('=====> AD_PAUSE', e)
 })
 
+player.on(AdEvents.IMA_AD_LOADER_READY, function (e) {
+  // Request video ads.
+  const adsRequest = new google.ima.AdsRequest()
+  adsRequest.adTagUrl =
+    'https://pubads.g.doubleclick.net/gampad/ads?' +
+    'iu=/21775744923/external/single_ad_samples&sz=640x480&' +
+    'cust_params=sample_ct%3Dlinear&ciu_szs=300x250%2C728x90&gdfp_req=1&' +
+    'output=vast&unviewed_position_start=1&env=vp&impl=s&correlator='
 
+  // Specify the linear and nonlinear slot sizes. This helps the SDK to
+  // select the correct creative if multiple are returned.
+  adsRequest.linearAdSlotWidth = 640
+  adsRequest.linearAdSlotHeight = 400
 
-// Request video ads.
-const adsRequest = new google.ima.AdsRequest()
-adsRequest.adTagUrl = 'https://pubads.g.doubleclick.net/gampad/ads?' +
-  'iu=/21775744923/external/single_ad_samples&sz=640x480&' +
-  'cust_params=sample_ct%3Dlinear&ciu_szs=300x250%2C728x90&gdfp_req=1&' +
-  'output=vast&unviewed_position_start=1&env=vp&impl=s&correlator='
+  adsRequest.nonLinearAdSlotWidth = 640
+  adsRequest.nonLinearAdSlotHeight = 150
 
-// Specify the linear and nonlinear slot sizes. This helps the SDK to
-// select the correct creative if multiple are returned.
-adsRequest.linearAdSlotWidth = 640
-adsRequest.linearAdSlotHeight = 400
-
-adsRequest.nonLinearAdSlotWidth = 640
-adsRequest.nonLinearAdSlotHeight = 150
-
-player.plugins.ad.requestAd(adsRequest)
+  player.plugins.ad.requestAds(adsRequest)
+})
---
+++ packages/xgplayer-ads/src/plugin.js
@@ -0,0 +1,118 @@
+import { BasePlugin, Plugin, Util } from 'xgplayer'
+import { Logger, createPublicPromise } from 'xgplayer-streaming-shared'
+import * as AdEvents from './events'
+import { ImaAdManager } from './imaAdManager'
+import './index.scss'
+
+const logger = new Logger('AdsPlugin')
+
+export class AdsPlugin extends Plugin {
+  static get pluginName () {
+    return 'ad'
+  }
+  get version () {
+    return __VERSION__
+  }
+
+  afterCreate () {
+    this.csManager = undefined
+  }
+
+  beforePlayerInit () {
+    this._initHooks()
+    this._proxyPlayer()
+
+    this.initPromise = createPublicPromise()
+
+    if (this.config.adType === 'ima') {
+      this.initClientSideAd()
+    }
+
+    return this.initPromise
+  }
+
+  /**
+   * @private
+   */
+  _initHooks () {
+    this.player.useHooks('play', () => {
+      if (this.csManager?.isAdRunning) {
+        this.csManager?.play()
+        return false
+      }
+    })
+    this.player.useHooks('pause', () => {
+      if (this.csManager?.isAdRunning) {
+        this.csManager?.pause()
+        return false
+      }
+    })
+  }
+
+  /**
+   * @private
+   */
+  _proxyPlayer () {
+    const { player } = this
+
+    BasePlugin.defineGetterOrSetter(player, {
+      adPaused: {
+        get: () => {
+          const media = player.media || player.video
+          logger.log(
+            'csManager.paused',
+            this.csManager?.isAdRunning,
+            this.csManager.paused,
+            this.csManager?.isAdRunning
+              ? this.csManager.paused
+              : media
+                ? media.paused
+                : true
+          )
+          return this.csManager?.isAdRunning
+            ? this.csManager.paused
+            : media
+              ? media.paused
+              : true
+        },
+        configurable: true
+      }
+    })
+  }
+
+  render () {
+    return Util.createDom('xg-ad', '', {}, 'xgplayer-ads')
+  }
+
+  initClientSideAd () {
+    if (this.config.adType === 'ima') {
+      this.initImaAd()
+    }
+  }
+
+  initImaAd () {
+    this.csManager = new ImaAdManager({
+      displayContainer: this.root,
+      player: this.player
+    })
+
+    this.csManager.on(AdEvents.IMA_AD_LOADER_READY, () => {
+      this.emit(AdEvents.IMA_AD_LOADER_READY)
+    })
+    this.csManager.on(AdEvents.IMA_AD_MANAGER_READY, () => {
+      this.initPromise?.resolve()
+      this.emit(AdEvents.IMA_AD_MANAGER_READY)
+    })
+
+
+    this.csManager.init()
+  }
+
+  requestAds (payload) {
+    this.csManager?.requestAds(payload)
+  }
+
+  destroy () {
+    this.csManager?.destroy()
+  }
+}
