26804f0af7ef9a173c1b8b5ffb4c308fb0312ae1
gemstone <gems.xu@gmail.com>
2024-06-18 18:16:27 +0800

    docs: ✏️ (xgplayer-ads) 更新IMA使用方式

--- fixtures/ads/index.js
+++ fixtures/ads/index.js
@@ -2,46 +2,113 @@ import AdPlugin, { AdEvents } from '../../packages/xgplayer-ads/src'
 import { Logger } from '../../packages/xgplayer-streaming-shared/src/logger'
 import Player from '../../packages/xgplayer/src/index.umd'
 
+let settings = {}
+const dbApplyOpt = document.getElementById('apply-opt')
+const doPresetUrl = document.getElementById('ima-preset-url')
+const doAdsRequest = document.getElementById('ima-ads-request')
+const doAdsResponse = document.getElementById('ima-ads-response')
+
 // TODO: delete
 Logger.enable()
 
-window.player = new Player({
-  id: 'video',
-  url: '//lf3-static.bytednsdoc.com/obj/eden-cn/nupenuvpxnuvo/xgplayer_doc/xgplayer-demo-720p.mp4',
-  autoplay: true,
-  width: '80%',
-  height: 700,
-  ignores: [],
-  plugins: [AdPlugin],
-  ad: {
-    adType: 'ima'
+function generateAdTagUrl() {
+  return doPresetUrl.getElementsByTagName('input')[0].value
+}
+
+function generateAdsRequest(settings) {
+  if (settings.useAdsRequest) {
+    // Request video ads.
+    const adsRequest = new google.ima.AdsRequest()
+    adsRequest.adTagUrl = generateAdTagUrl()
+
+    // Specify the linear and nonlinear slot sizes. This helps the SDK to
+    // select the correct creative if multiple are returned.
+    adsRequest.linearAdSlotWidth = 640
+    adsRequest.linearAdSlotHeight = 400
+
+    adsRequest.nonLinearAdSlotWidth = 640
+    adsRequest.nonLinearAdSlotHeight = 150
+
+    return adsRequest
   }
+}
+
+function generateAdsResponse() {
+  // Response video ads.
+}
+
+function initSetting() {
+  function defaultOpt() {
+    return {
+      useAdsRequest: true,
+      useAdsResponse: false
+    }
+  }
+  var cachedOpt = localStorage.getItem('xg:test:ads:opt')
+  try {
+    cachedOpt = JSON.parse(cachedOpt)
+  } catch (error) {
+    cachedOpt = undefined
+  }
+  var opts = Object.assign({}, defaultOpt(), cachedOpt, {
+    useAdsRequest: doAdsRequest.getElementsByTagName('input')[0].value,
+    useAdsResponse: doAdsResponse.getElementsByTagName('input')[0].value,
+    adTagUrl: doPresetUrl.getElementsByTagName('input')[0].value
+  })
+
+  return opts
+}
+
+function newPlayer () {
+  window.player = new Player({
+    id: 'video',
+    url: '//lf3-static.bytednsdoc.com/obj/eden-cn/nupenuvpxnuvo/xgplayer_doc/xgplayer-demo-720p.mp4',
+    autoplay: true,
+    width: '80%',
+    height: 700,
+    plugins: [AdPlugin],
+    ad: {
+      adType: 'ima',
+      ima: {
+        adsRequest: generateAdsRequest(settings),
+        adsResponse: settings.adsResponse,
+        adTagUrl: settings.adTagUrl
+      }
+    }
+  })
+
+  player.on(AdEvents.AD_PLAY, function (e) {
+    console.log('=====> AD_PLAY', e)
+  })
+
+  player.on(AdEvents.AD_PAUSE, function (e) {
+    console.log('=====> AD_PAUSE', e)
+  })
+}
+
+function initPlayer() {
+  settings = initSetting()
+
+  if (settings.useAdsResponse && settings.adTagUrl) {
+    fetch(settings.adTagUrl)
+      .then(res => res.text())
+      .then(data => {
+        settings.adsResponse = data
+        newPlayer()
+      })
+  } else {
+    newPlayer()
+  }
+}
+
+dbApplyOpt.addEventListener('click', function () {
+  initPlayer()
 })
 
-player.on(AdEvents.AD_PLAY, function (e) {
-  console.log('=====> AD_PLAY', e)
-})
-
-player.on(AdEvents.AD_PAUSE, function (e) {
-  console.log('=====> AD_PAUSE', e)
-})
-
-player.on(AdEvents.IMA_AD_LOADER_READY, function (e) {
-  // Request video ads.
-  const adsRequest = new google.ima.AdsRequest()
-  adsRequest.adTagUrl =
-    'https://pubads.g.doubleclick.net/gampad/ads?' +
-    'iu=/21775744923/external/single_ad_samples&sz=640x480&' +
-    'cust_params=sample_ct%3Dlinear&ciu_szs=300x250%2C728x90&gdfp_req=1&' +
-    'output=vast&unviewed_position_start=1&env=vp&impl=s&correlator='
-
-  // Specify the linear and nonlinear slot sizes. This helps the SDK to
-  // select the correct creative if multiple are returned.
-  adsRequest.linearAdSlotWidth = 640
-  adsRequest.linearAdSlotHeight = 400
-
-  adsRequest.nonLinearAdSlotWidth = 640
-  adsRequest.nonLinearAdSlotHeight = 150
-
-  player.plugins.ad.requestAds(adsRequest)
-})
+doPresetUrl.getElementsByTagName('select')[0].onchange = function () {
+  if (this.value) {
+    const input = doPresetUrl.getElementsByTagName('input')[0]
+    input.value = this.value
+    initPlayer()
+  }
+}
